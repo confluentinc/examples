2a959f78831257ba7c3563d9cea11c3c5d71eb25
Yeva Byzek <yeva@confluent.io>
2018-04-30 14:26:42 -0400

    DEVX-85: standalone script for generating component config deltas from Confluent Cloud configs

---
+++ ccloud/ccloud-generate-cp-configs.sh
@@ -0,0 +1,75 @@
+#!/bin/bash
+
+# Confluent Cloud configuration
+CCLOUD_CONFIG=$HOME/.ccloud/config
+
+# Destination directory
+if [[ ! -z "$1" ]]; then
+  DEST=$1
+else
+  DEST="delta_configs"
+fi
+rm -fr $DEST
+mkdir -p $DEST
+
+if [[ ! -e $CCLOUD_CONFIG ]]; then
+  echo "'ccloud' is not initialized. Run 'ccloud init' and try again"
+  exit 1
+fi
+
+# Build configuration file with CCloud connection parameters and
+# Confluent Monitoring Interceptors for Streams Monitoring in Confluent Control Center
+INTERCEPTORS_CCLOUD_CONFIG=$DEST/interceptors-ccloud.config
+while read -r line
+do
+  echo $line >> $INTERCEPTORS_CCLOUD_CONFIG
+  if [[ ${line:0:4} == 'sasl' || ${line:0:3} == 'ssl' || ${line:0:8} == 'security' || ${line:0:9} == 'bootstrap' ]]; then
+    echo "confluent.monitoring.interceptor.$line" >> $INTERCEPTORS_CCLOUD_CONFIG
+  fi
+done < "$CCLOUD_CONFIG"
+
+# Confluent Schema Registry instance for Confluent Cloud
+SR_CONFIG_DELTA=$DEST/schema-registry-ccloud.delta
+while read -r line
+do
+  if [[ ! -z $line && ${line:0:1} != '#' ]]; then
+    if [[ ${line:0:9} == 'bootstrap' && ! "$line" =~ "SASL_SSL:" ]]; then
+      # Schema Registry requires security protocol, i.e. "SASL_SSL://", in kafkastore.bootstrap.servers
+      # Workaround until this issue is resolved https://github.com/confluentinc/schema-registry/issues/790
+      line=${line/=/=SASL_SSL:\/\/}
+      line=${line/,/,SASL_SSL:\/\/}
+    fi
+    echo "kafkastore.$line" >> $SR_CONFIG_DELTA
+  fi
+done < "$CCLOUD_CONFIG"
+
+
+# KSQL DataGen for Confluent Cloud
+KSQL_DATAGEN_DELTA=$DEST/ksql-datagen.delta
+cp $INTERCEPTORS_CCLOUD_CONFIG $KSQL_DATAGEN_DELTA
+echo "interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor" >> $KSQL_DATAGEN_DELTA
+
+
+# Confluent Replicator for Confluent Cloud
+REPLICATOR_PRODUCER_DELTA=$DEST/replicator-to-ccloud-producer.delta
+cp $INTERCEPTORS_CCLOUD_CONFIG $REPLICATOR_PRODUCER_DELTA
+echo "interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor" >> $REPLICATOR_PRODUCER_DELTA
+
+
+# KSQL Server runs locally and connects to Confluent Cloud
+KSQL_SERVER_DELTA=$DEST/ksql-server-ccloud.delta
+cp $INTERCEPTORS_CCLOUD_CONFIG $KSQL_SERVER_DELTA
+echo "producer.interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor" >> $KSQL_SERVER_DELTA
+echo "consumer.interceptor.classes=io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor" >> $KSQL_SERVER_DELTA
+
+# Confluent Control Center runs locally, monitors Confluent Cloud, and uses Confluent Cloud cluster as the backstore
+C3_DELTA=$DEST/control-center-ccloud.delta
+while read -r line
+  do
+  if [[ ! -z $line && ${line:0:1} != '#' ]]; then
+    echo "$line" >> $C3_DELTA
+    if [[ ${line:0:4} == 'sasl' || ${line:0:3} == 'ssl' || ${line:0:8} == 'security' || ${line:0:9} == 'bootstrap' ]]; then
+      echo "confluent.controlcenter.streams.$line" >> $C3_DELTA
+    fi
+  fi
+done < "$CCLOUD_CONFIG"
