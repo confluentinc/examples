c4040853d4a88ad5f3eb110efb2a01450e387dcd
Yeva Byzek <yeva@confluent.io>
2018-06-21 22:41:07 -0400

    Temporary workaround for AvroConverter issue

--- ccloud/start.sh
+++ ccloud/start.sh
@@ -43,6 +43,9 @@ kafka-topics --zookeeper localhost:2181 --create --topic pageviews --partitions 
 ksql-datagen quickstart=pageviews format=avro topic=pageviews maxInterval=100 schemaRegistryUrl=http://localhost:$SR_LISTENER &>/dev/null &
 sleep 5
 
+# Temporary workaround for AvroConverter issue
+curl -X POST -H "Content-Type: application/vnd.schemaregistry.v1+json" --data '{"schema": "{\"type\":\"record\",\"name\":\"ConnectDefault\",\"namespace\":\"io.confluent.connect.avro\",\"fields\":[{\"name\":\"viewtime\",\"type\":\"long\"},{\"name\":\"userid\",\"type\":\"string\"},{\"name\":\"pageid\",\"type\":\"string\"}]}"}' http://localhost:$SR_LISTENER/subjects/pageviews.replica-value/versions
+
 # Produce to topic users in CCloud cluster
 ccloud topic create users
 KSQL_DATAGEN_PROPERTIES=$CONFLUENT_CURRENT/ksql-server/ksql-datagen.properties
