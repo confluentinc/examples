6fa464502d063d47d5e940025dc61f47775d68c4
Robin Moffatt <robin@confluent.io>
2018-07-12 13:41:11 -0700

    Add Postgres demo

---
+++ postgres-debezium-ksql-elasticsearch/README.md
@@ -0,0 +1,3 @@
+see [Demo Script](postgres-debezium-ksql-elasticsearch-docker-short.adoc)
+
+Slides: https://speakerdeck.com/rmoff/streaming-etl-in-practice-with-postgresql-apache-kafka-and-ksql
---
+++ postgres-debezium-ksql-elasticsearch/start.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+echo '\n\n******\nThis demo cannot be run from confluent CLI.\n******\n\nPlease see postgres-debezium-ksql-elasticsearch-docker-short.adoc for step-by-step instructions on how to run this demo.\n\n'
