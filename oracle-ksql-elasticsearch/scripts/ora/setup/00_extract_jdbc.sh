b094187402667384b7e8bb1511e8e2ba61cb7916
Robin Moffatt <robin@confluent.io>
2018-07-12 13:32:22 -0700

    Add Oracle demo

---
+++ oracle-ksql-elasticsearch/scripts/ora/setup/00_extract_jdbc.sh
@@ -0,0 +1,1 @@
+cp -v /opt/oracle/product/12.2.0.1/dbhome_1/inventory/Scripts/ext/jlib/ojdbc8.jar /jars
---
+++ oracle-ksql-elasticsearch/scripts/ora/setup/01_swingbench_dl.sh
@@ -0,0 +1,1 @@
+curl http://dominicgiles.com/swingbench/swingbench261082.zip -o swingbench.zip && unzip swingbench.zip
