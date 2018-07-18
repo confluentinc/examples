ae4df983df939884cc1fe8d9e71bbbb11bfb25b7
Yeva Byzek <yeva@confluent.io>
2018-07-18 15:43:52 -0400

    Create topics; consume topics; check REST port and sleep 10 seconds

--- microservices-orders/start.sh
+++ microservices-orders/start.sh
@@ -5,7 +5,7 @@
 
 check_env || exit 1
 check_mvn || exit 1
-check_running_cp 5.0 || exit 
+check_running_cp 5.0 || exit 1
 
 ./stop.sh
 
@@ -15,10 +15,27 @@ sleep 5
 [[ -d "kafka-streams-examples" ]] || git clone https://github.com/confluentinc/kafka-streams-examples.git
 (cd kafka-streams-examples && git checkout DEVX-147 && mvn clean compile -DskipTests)
 
-RESTPORT=53231
+# Create topics
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic orders
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic payments
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic customers
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic order-validations
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic warehouse-inventory
+
+RESTPORT=$(jot -r 1  10000 65000)
+
+# Dlog4j.configuration=src/main/resources/log4j.properties
 
 echo "Starting OrdersService"
 mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.OrdersService -Dexec.args="localhost:9092 http://localhost:8081 localhost $RESTPORT" > /dev/null 2>&1 &
+
+sleep 10
+if [[ $(netstat -ant | grep $RESTPORT) == "" ]]; then
+  echo "OrdersService not running on port $RESTPORT.  Please troubleshoot"
+  exit 1
+else
+  echo "OrdersService running on port $RESTPORT"
+fi
 
 echo "Starting InventoryService"
 mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.InventoryService > /dev/null 2>&1 &
@@ -42,5 +59,8 @@ mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.ex
 
 # Validate messages in topics
 confluent consume orders --value-format avro --max-messages 5
+confluent consume payments --value-format avro --max-messages 5
+confluent consume customers --value-format avro --max-messages 5
+confluent consume order-validations --value-format avro --max-messages 5
 confluent consume warehouse-inventory --max-messages 5
 
