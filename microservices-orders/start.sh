47137db4f9f353f23ed71d7f003e93afbf5e26a8
Yeva Byzek <yeva@confluent.io>
2018-07-18 17:15:18 -0400

    Optimize start.sh; test a few times to find free port

--- microservices-orders/start.sh
+++ microservices-orders/start.sh
@@ -15,14 +15,25 @@ sleep 5
 [[ -d "kafka-streams-examples" ]] || git clone https://github.com/confluentinc/kafka-streams-examples.git
 (cd kafka-streams-examples && git checkout DEVX-147 && mvn clean compile -DskipTests)
 
+# Get random port number
+RESTPORT=$(jot -r 1  10000 65000)
+COUNT=0
+while [[ $(netstat -ant | grep $RESTPORT) != "" ]]; do
+  RESTPORT=$(jot -r 1  10000 65000)
+  COUNT=$((COUNT+1))
+  if [[ $COUNT > 5 ]]; then
+    echo "Could not allocate a free rest port. Please troubleshoot"
+    exit 1
+  fi
+done
+echo "Port tcp:$RESTPORT looks free"
+
 # Create topics
 kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic orders
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic payments
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic customers
 kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic order-validations
 kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic warehouse-inventory
-
-RESTPORT=$(jot -r 1  10000 65000)
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic payments
+kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic customers
 
 # Dlog4j.configuration=src/main/resources/log4j.properties
 
@@ -33,34 +44,27 @@ sleep 10
 if [[ $(netstat -ant | grep $RESTPORT) == "" ]]; then
   echo "OrdersService not running on port $RESTPORT.  Please troubleshoot"
   exit 1
-else
-  echo "OrdersService running on port $RESTPORT"
 fi
 
-echo "Starting InventoryService"
-mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.InventoryService > /dev/null 2>&1 &
+for SERVICE in "InventoryService" "FraudService" "OrderDetailsService" "ValidationsAggregatorService" "EmailService"; do
+    echo "Starting $SERVICE"
+    mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.$SERVICE > /dev/null 2>&1 &
+done
 
-echo "Starting FraudService"
-mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.FraudService > /dev/null 2>&1 &
-
-echo "Starting OrderDetailsService"
-mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.OrderDetailsService > /dev/null 2>&1 &
-
-echo "Starting ValidationsAggregatorService"
-mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.ValidationsAggregatorService > /dev/null 2>&1 &
-
-echo "Starting EmailService"
-mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.EmailService > /dev/null 2>&1 &
-
-sleep 5
+sleep 10
 
 echo "Posting Order Requests"
 mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.PostOrderRequests -Dexec.args="$RESTPORT" > /dev/null 2>&1 &
 
 # Validate messages in topics
+echo "orders:"
 confluent consume orders --value-format avro --max-messages 5
-confluent consume payments --value-format avro --max-messages 5
-confluent consume customers --value-format avro --max-messages 5
+echo "order-validations:"
 confluent consume order-validations --value-format avro --max-messages 5
+echo "warehouse-inventory:"
 confluent consume warehouse-inventory --max-messages 5
+echo "payments:"
+confluent consume payments --value-format avro --max-messages 5
+echo "customers:"
+confluent consume customers --value-format avro --max-messages 5
 
