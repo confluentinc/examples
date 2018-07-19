670f16aa4bb345f7f8b08d3ef7886c738a037278
Yeva Byzek <yeva@confluent.io>
2018-07-18 21:46:42 -0400

    AddInventory.java ; inventory-service-store-of-reserved-stock-changelog

--- microservices-orders/start.sh
+++ microservices-orders/start.sh
@@ -9,10 +9,11 @@ check_running_cp 5.0 || exit 1
 
 ./stop.sh
 
-confluent start schema-registry
+confluent start
 sleep 5
 
 [[ -d "kafka-streams-examples" ]] || git clone https://github.com/confluentinc/kafka-streams-examples.git
+cp -n AddInventory.java kafka-streams-examples/src/main/java/io/confluent/examples/streams/microservices/.
 (cd kafka-streams-examples && git checkout DEVX-147 && mvn clean compile -DskipTests)
 
 # Get random port number
@@ -45,6 +46,9 @@ if [[ $(netstat -ant | grep $RESTPORT) == "" ]]; then
   exit 1
 fi
 
+echo "Adding Inventory"
+mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.AddInventory -Dexec.args="75 20" > /dev/null 2>&1 &
+
 for SERVICE in "InventoryService" "FraudService" "OrderDetailsService" "ValidationsAggregatorService"; do
     echo "Starting $SERVICE"
     mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.$SERVICE > /dev/null 2>&1 &
@@ -57,13 +61,22 @@ mvn exec:java -f kafka-streams-examples/pom.xml -Dexec.mainClass=io.confluent.ex
 
 sleep 10
 
-# Validate messages in topics
+# Topic orders: a unique order is requested 1 per second
 echo "-----orders-----"
 confluent consume orders --value-format avro --max-messages 5
+
+# Topic order-validations: PASS/FAIL for each "checkType": ORDER_DETAILS_CHECK (OrderDetailsService), FRAUD_CHECK (FraudService), INVENTORY_CHECK (InventoryService)
 echo "-----order-validations-----"
 confluent consume order-validations --value-format avro --max-messages 5
+
+# Topic warehouse-inventory: initial inventory in stock
 echo "-----warehouse-inventory-----"
 confluent consume warehouse-inventory --max-messages 2 --from-beginning --property print.key=true --property value.deserializer=org.apache.kafka.common.serialization.IntegerDeserializer
+
+# Topic inventory-service-store-of-reserved-stock-changelog: table backing the reserved inventory
+# It maxes out when orders = initial inventory
+echo "-----inventory-service-store-of-reserved-stock-changelog-----"
+confluent consume inventory-service-store-of-reserved-stock-changelog --property print.key=true --property value.deserializer=org.apache.kafka.common.serialization.LongDeserializer -from-beginning --max-messages 20
 
 # Requires EmailService to be populated with customer info
 #echo "-----payments-----"
