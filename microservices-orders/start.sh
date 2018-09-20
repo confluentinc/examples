f4e31de895ff9738e476de01bc889c22bf35b732
ybyzek <ybyzek@users.noreply.github.com>
2018-09-20 18:39:31 -0400

    DEVX-265: move java code to kafka-streams-examples (#45)

--- microservices-orders/start.sh
+++ microservices-orders/start.sh
@@ -15,7 +15,6 @@ check_running_cp 5.0 || exit 1
 
 # Compile java client code
 [[ -d "kafka-streams-examples" ]] || git clone https://github.com/confluentinc/kafka-streams-examples.git
-yes | cp -f src/main/java/*.java kafka-streams-examples/src/main/java/io/confluent/examples/streams/microservices/.
 (cd kafka-streams-examples && git fetch && git checkout 5.0.0-post && git pull && mvn clean compile -DskipTests)
 if [[ $? != 0 ]]; then
   echo "There seems to be a BUILD FAILURE error? Please troubleshoot"
