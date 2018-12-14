bf3c0abc9dec3bc7f7d66e5bf6dcb63cadcb9c60
Robin Moffatt <robin@confluent.io>
2018-12-14 13:09:25 +0000

    Add console output to script

--- clickstream/start_datagen.sh
+++ clickstream/start_datagen.sh
@@ -4,7 +4,11 @@
 . ../utils/helper.sh
 
 if is_ce; then PROPERTIES=" propertiesFile=$CONFLUENT_HOME/etc/ksql/datagen.properties"; else PROPERTIES=""; fi
+echo -e "\nStarting data generators…"
 ksql-datagen -daemon quickstart=clickstream format=json topic=clickstream maxInterval=100 iterations=500000 $PROPERTIES &>/dev/null &
+echo -e "…"
 ksql-datagen quickstart=clickstream_codes format=json topic=clickstream_codes maxInterval=20 iterations=100 $PROPERTIES &>/dev/null &
+echo -e "…"
 ksql-datagen quickstart=clickstream_users format=json topic=clickstream_users maxInterval=10 iterations=1000 $PROPERTIES &>/dev/null &
 sleep 5
+echo -e "…done\n"
