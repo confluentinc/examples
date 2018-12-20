ffaccc3ede7c42b6321455b3f5facb2db044d9c4
Dan <dan@confluent.io>
2018-12-20 09:06:41 +0000

    more idiomatic Streams

--- clients/cloud/scala/src/main/scala/io/confluent/examples/clients/scala/Streams.scala
+++ clients/cloud/scala/src/main/scala/io/confluent/examples/clients/scala/Streams.scala
@@ -1,5 +1,5 @@
 /**
-  * Copyright 2015 Confluent Inc.
+  * Copyright 2018 Confluent Inc.
   *
   * Licensed under the Apache License, Version 2.0 (the "License");
   * you may not use this file except in compliance with the License.
@@ -16,44 +16,45 @@
 package io.confluent.examples.clients.scala
 
 import java.io.FileReader
-import java.util
 import java.util.{Collections, Properties}
 
 import io.confluent.examples.clients.scala.model.RecordJSON
-import org.apache.kafka.common.serialization.{Deserializer, Serde, Serdes, Serializer}
-import io.confluent.kafka.serializers.{KafkaJsonDeserializer, KafkaJsonSerializer}
-import org.apache.kafka.streams.{KafkaStreams, KeyValue, StreamsBuilder}
-import org.apache.kafka.streams.kstream._
+
+import org.apache.kafka.common.serialization.Serde
+import org.apache.kafka.streams.KafkaStreams
+import org.apache.kafka.streams.kstream.Printed
+import org.apache.kafka.streams.scala.ImplicitConversions._
+import org.apache.kafka.streams.scala._
+import org.apache.kafka.streams.scala.kstream._
 
 import scala.util.Random
 
 object Streams extends App {
+
+  import Serdes._
+
   val configFileName = args(0)
   val topicName = args(1)
   val props = buildProperties(configFileName)
 
-  val RecordJSON: Serde[RecordJSON] = getJsonSerde()
-
-  val builder = new StreamsBuilder
-
-  val records :KStream[String, RecordJSON] = builder.stream(topicName, Consumed.`with`(Serdes.String, RecordJSON))
-  val counts :KStream[String,Long] = records.map((k: String, v: RecordJSON) => new KeyValue[String, Long](k, v.getCount))
-  counts.print(Printed.toSysOut[String,Long].withLabel("Consumed record"))
+  implicit val recordJSONSerde: Serde[RecordJSON] = getJsonSerde
+  val builder: StreamsBuilder = new StreamsBuilder
+  val records: KStream[String, RecordJSON] = builder.stream[String, RecordJSON](topicName)
+  val counts: KStream[String, Long] = records.map((k, v) => (k, v.getCount))
+  counts.print(Printed.toSysOut[String, Long].withLabel("Consumed record"))
 
   // Aggregate values by key
-  val countAgg :KStream[String,Long] = counts.groupByKey()
+  val countAgg: KStream[String, Long] = counts.groupByKey
     .reduce((aggValue, newValue) => aggValue + newValue)
-    .toStream()
-  countAgg.print(Printed.toSysOut[String,Long].withLabel("Running count"))
+    .toStream
+
+  countAgg.print(Printed.toSysOut[String, Long].withLabel("Running count"))
 
   val streams = new KafkaStreams(builder.build, props)
   streams.start()
 
   // Add shutdown hook to respond to SIGTERM and gracefully close Kafka Streams
-  Runtime.getRuntime.addShutdownHook(new Thread(new Runnable(){override def run{streams.close}}))
-
-
-  /*?************/
+  Runtime.getRuntime.addShutdownHook(new Thread(new Runnable() { override def run { streams.close }}))
 
   def buildProperties(configFileName: String): Properties = {
     import org.apache.kafka.clients.consumer.ConsumerConfig
@@ -64,20 +65,21 @@ object Streams extends App {
     properties.put(StreamsConfig.CACHE_MAX_BYTES_BUFFERING_CONFIG, "0")
     properties.put(StreamsConfig.REPLICATION_FACTOR_CONFIG, "3")
     properties.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest")
-    properties.put(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG, Serdes.String().getClass)
-    properties.put(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG, Serdes.Long().getClass)
     properties.load(new FileReader(configFileName))
     properties
   }
 
-
-  private def getJsonSerde():Serde[RecordJSON]  = {
+  private def getJsonSerde(): Serde[RecordJSON] = {
+    import org.apache.kafka.common.serialization.Serdes
+    import io.confluent.kafka.serializers.{KafkaJsonDeserializer, KafkaJsonSerializer}
+    import org.apache.kafka.common.serialization.{Serializer,Deserializer}
     val serdeProps: java.util.Map[String, Object] = Collections.singletonMap("json.value.type", classOf[RecordJSON])
-    val mySerializer:Serializer[RecordJSON] =  new KafkaJsonSerializer[RecordJSON]()
+    val mySerializer: Serializer[RecordJSON] = new KafkaJsonSerializer[RecordJSON]()
     mySerializer.configure(serdeProps, false)
-    val myDeserializer:Deserializer[RecordJSON] = new KafkaJsonDeserializer[RecordJSON]()
+    val myDeserializer: Deserializer[RecordJSON] = new KafkaJsonDeserializer[RecordJSON]()
     myDeserializer.configure(serdeProps, false)
     Serdes.serdeFrom(mySerializer, myDeserializer)
   }
+
 
 }
