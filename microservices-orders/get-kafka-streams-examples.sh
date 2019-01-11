d195926669b35cb16b4d20702ad26d74e3913a93
Yeva Byzek <yeva@confluent.io>
2019-01-11 14:44:59 -0500

    Revert get-kafka-streams-examples.sh to 5.1.0-post

--- microservices-orders/get-kafka-streams-examples.sh
+++ microservices-orders/get-kafka-streams-examples.sh
@@ -2,8 +2,7 @@
 
 # Compile java client code
 [[ -d "kafka-streams-examples" ]] || git clone https://github.com/confluentinc/kafka-streams-examples.git
-#(cd kafka-streams-examples && git fetch && git checkout 5.1.0-post && git pull && mvn clean compile -DskipTests package)
-(cd kafka-streams-examples && mvn clean compile -DskipTests package)
+(cd kafka-streams-examples && git fetch && git checkout 5.1.0-post && git pull && mvn clean compile -DskipTests package)
 if [[ $? != 0 ]]; then
   echo "ERROR: There seems to be a BUILD FAILURE error? Please troubleshoot"
   exit 1
