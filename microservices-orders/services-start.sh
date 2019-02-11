611ebe14024e6e2196362dab519076f68398bd01
Yeva Byzek <yeva@confluent.io>
2019-02-11 09:29:05 -0500

    DEVX-703: bump from 5.1.0 to 5.1.1

--- microservices-orders/services-start.sh
+++ microservices-orders/services-start.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 RESTPORT=18894
+JAR=/usr/share/java/kafka-streams-examples/kafka-streams-examples-5.1.1-standalone.jar
 
-java -cp /usr/share/java/kafka-streams-examples/kafka-streams-examples-5.1.0-standalone.jar io.confluent.examples.streams.microservices.OrdersService broker:9092 http://schema-registry:8081 localhost $RESTPORT > /dev/null 2>&1 &
+java -cp $JAR io.confluent.examples.streams.microservices.OrdersService broker:9092 http://schema-registry:8081 localhost $RESTPORT > /dev/null 2>&1 &
 
 sleep 10
 
-java -cp /usr/share/java/kafka-streams-examples/kafka-streams-examples-5.1.0-standalone.jar io.confluent.examples.streams.microservices.AddInventory 20 20 broker:9092 > /dev/null 2>&1 &
+java -cp $JAR io.confluent.examples.streams.microservices.AddInventory 20 20 broker:9092 > /dev/null 2>&1 &
 
 for SERVICE in "InventoryService" "FraudService" "OrderDetailsService" "ValidationsAggregatorService" "EmailService"; do
   echo "Starting $SERVICE"
-  java -cp /usr/share/java/kafka-streams-examples/kafka-streams-examples-5.1.0-standalone.jar io.confluent.examples.streams.microservices.$SERVICE broker:9092 http://schema-registry:8081 > /dev/null 2>&1 &
+  java -cp $JAR io.confluent.examples.streams.microservices.$SERVICE broker:9092 http://schema-registry:8081 > /dev/null 2>&1 &
 done
 
 sleep 10
 
-java -cp /usr/share/java/kafka-streams-examples/kafka-streams-examples-5.1.0-standalone.jar io.confluent.examples.streams.microservices.PostOrdersAndPayments $RESTPORT broker:9092 http://schema-registry:8081 > /dev/null 2>&1 &
+java -cp $JAR io.confluent.examples.streams.microservices.PostOrdersAndPayments $RESTPORT broker:9092 http://schema-registry:8081 > /dev/null 2>&1 &
