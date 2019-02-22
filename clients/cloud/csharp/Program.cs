4215306abd523a43179051ab6d9dc71beaddf610
matt <matt@confluent.io>
2019-02-21 22:25:50 -0800

    csharp example

---
+++ clients/cloud/csharp/Program.cs
@@ -0,0 +1,183 @@
+﻿using Confluent.Kafka;
+using Confluent.Kafka.Admin;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+
+namespace CCloud
+{
+    class Program
+    {
+        static string ExtractJaasValue(string jaasConfig, string key)
+        {
+            var beginToken = key + "\\=\"";
+            var startIdx = jaasConfig.IndexOf(beginToken);
+            var endIdx = jaasConfig.IndexOf("\"", startIdx + beginToken.Length);
+            return jaasConfig.Substring(startIdx + beginToken.Length, endIdx - startIdx - beginToken.Length);
+        }
+
+        static async Task<ClientConfig> LoadConfig(string configPath, string certDir)
+        {
+            try
+            {
+                var cloudConfig = (await File.ReadAllLinesAsync(configPath))
+                    .Where(line => !line.StartsWith("#"))
+                    .ToDictionary(
+                        line => line.Substring(0, line.IndexOf('=')),
+                        line => line.Substring(line.IndexOf('=') + 1));
+
+                var clientConfig = new ClientConfig
+                {
+                    BootstrapServers = cloudConfig["bootstrap.servers"].Replace("\\", ""),
+                    BrokerVersionFallback = "0.10.0.0",
+                    ApiVersionFallbackMs = 0,
+                    SaslMechanism = SaslMechanism.Plain,
+                    SecurityProtocol = SecurityProtocol.SaslSsl,
+                    SaslUsername = ExtractJaasValue(cloudConfig["sasl.jaas.config"], "username"),
+                    SaslPassword = ExtractJaasValue(cloudConfig["sasl.jaas.config"], "password")
+                };
+
+                if (certDir != null)
+                {
+                    clientConfig.SslCaLocation = certDir;
+                }
+
+                return clientConfig;
+            }
+            catch
+            {
+                Console.WriteLine($"An error occured reading the config file from: {configPath}");
+                System.Environment.Exit(1);
+                return null; // avoid not-all-paths-return-value compiler error.
+            }
+        }
+
+        static async Task CreateTopicMaybe(string name, int numPartitions, short replicationFactor, ClientConfig cloudConfig)
+        {
+            using (var adminClient = new AdminClientBuilder(cloudConfig).Build())
+            {
+                try
+                {
+                    await adminClient.CreateTopicsAsync(new List<TopicSpecification> {
+                        new TopicSpecification { Name = name, NumPartitions = numPartitions, ReplicationFactor = replicationFactor } });
+                }
+                catch (CreateTopicsException e)
+                {
+                    if (e.Results[0].Error.Code != ErrorCode.TopicAlreadyExists)
+                    {
+                        Console.WriteLine($"An error occured creating topic {name}: {e.Results[0].Error.Reason}");
+                    }
+                    else
+                    {
+                        Console.WriteLine("Topic already exists");
+                    }
+                }
+            }
+        }
+        
+        static void Produce(string topic, ClientConfig config)
+        {
+            using (var producer = new ProducerBuilder<string, string>(config).Build())
+            {
+                int numProduced = 0;
+                int numMessages = 10;
+                for (int i=0; i<numMessages; ++i)
+                {
+                    var key = "alice";
+                    var val = JObject.FromObject(new { count = i }).ToString(Formatting.None);
+
+                    Console.WriteLine($"Producing record: {key} {val}");
+
+                    producer.BeginProduce(topic, new Message<string, string> { Key = key, Value = val },
+                        (deliveryReport) =>
+                        {
+                            if (deliveryReport.Error.Code != ErrorCode.NoError)
+                            {
+                                Console.WriteLine($"Failed to deliver message: {deliveryReport.Error.Reason}");
+                            }
+                            else
+                            {
+                                Console.WriteLine($"Produced message to: {deliveryReport.TopicPartitionOffset}");
+                                numProduced += 1;
+                            }
+                        });
+                }
+
+                producer.Flush(TimeSpan.FromSeconds(10));
+
+                Console.WriteLine($"{numProduced} messages were produced to topic {topic}");
+            }
+        }
+
+        static void Consume(string topic, ClientConfig config)
+        {
+            var consumerConfig = new ConsumerConfig(config);
+            consumerConfig.GroupId = "dotnet-example-group-1";
+            consumerConfig.AutoOffsetReset = AutoOffsetReset.Earliest;
+            consumerConfig.EnableAutoCommit = false;
+
+            CancellationTokenSource cts = new CancellationTokenSource();
+            Console.CancelKeyPress += (_, e) => {
+                e.Cancel = true; // prevent the process from terminating.
+                cts.Cancel();
+            };
+
+            using (var consumer = new ConsumerBuilder<string, string>(consumerConfig).Build())
+            {
+                consumer.Subscribe(topic);
+                var totalCount = 0;
+                try
+                {
+                    while (true)
+                    {
+                        var cr = consumer.Consume();
+                        totalCount += JObject.Parse(cr.Value).Value<int>("count");
+                        Console.WriteLine($"Consumed record with key {cr.Key} and value {cr.Value}, and updated total count to {totalCount}");
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Ctrl-C was pressed.
+                    Console.WriteLine("closed");
+                    consumer.Close();
+                }
+            }
+        }
+
+        static async Task Main(string[] args)
+        {
+            if (args.Length != 3 && args.Length != 4) {
+                Console.WriteLine("usage: .. <produce|consume> topic configPath [certDir]");
+                System.Environment.Exit(1);
+            }
+            
+            var mode = args[0];
+            var topic = args[1];
+            var configPath = args[2];
+            var certDir = args.Length == 4 ? args[3] : null;
+
+            var config = await LoadConfig(configPath, certDir);
+
+            switch (mode)
+            {
+                case "produce":
+                    await CreateTopicMaybe(topic, 1, 3, config);
+                    Produce(topic, config);
+                    break;
+                case "consume":
+                    Consume(topic, config);
+                    break;
+                default:
+                    Console.WriteLine($"First command line argument must be 'produce' or 'consume'. It was: {mode}");
+                    System.Environment.Exit(1);
+                    break;
+            }
+        }
+    }
+}
---
+++ clients/cloud/csharp/README.md
@@ -0,0 +1,90 @@
+﻿# Overview
+
+Produce messages to and consume messages from [Confluent Cloud](https://www.confluent.io/confluent-cloud/) using the .NET Producer and Consumer.
+
+
+# Prerequisites
+
+* .NET Core 2.1 or higher to run the demo application.
+* [Confluent Cloud CLI](https://docs.confluent.io/current/cloud/cli/install.html) installed on your machine. It is provided as part of the Confluent Platform package or may be [installed separately](https://docs.confluent.io/current/cloud/cli/install.html).
+* Access to a [Confluent Cloud](https://www.confluent.io/confluent-cloud/) cluster
+* [Initialize](https://docs.confluent.io/current/cloud/cli/multi-cli.html#connect-ccloud-cli-to-a-cluster) your local Confluent Cloud configuration file using the `ccloud init` command, which creates the file at `$HOME/.ccloud/config` (`%HOMEPATH%/.ccloud/config` on Windows).
+
+# Example
+
+In this example, the producer writes records to a topic in Confluent Cloud. 
+Each record has a key representing a username (e.g. `alice`) and a value of a count, formatted as json (e.g. `{"count": 0}`).
+The consumer reads the same topic from Confluent Cloud and keeps a rolling sum of the counts as it processes each record.
+
+## Produce Records
+
+Run the example application, passing in arguments for (a) whether to produce or consume (produce) (b) the topic name (c) the local file with configuration parameters to connect to your Confluent Cloud instance and (d, Windows only) a local file with default trusted root CA certificates. 
+
+> Note: On Windows, default trusted root CA certificates - which are required for secure access to Confluent Cloud - are stored in the Windows Registry. The .NET library does not currently have the capability to access these certificates, so you will need to obtain them from somewhere else, for example use the cacert.pem file distributed with curl: https://curl.haxx.se/ca/cacert.pem. 
+
+
+```shell
+# Build the client example application
+$ dotnet build
+
+# Run the producer (Windows)
+$ dotnet run produce test1 %HOMEPATH%/.ccloud/config /path/to/curl/cacert.pem
+
+# Run the producer (other)
+$ dotnet run produce test1 $HOME/.ccloud/config
+```
+
+You should see:
+
+```shell
+Producing record: alice	{"count":0}
+Producing record: alice	{"count":1}
+Producing record: alice	{"count":2}
+Producing record: alice	{"count":3}
+Producing record: alice	{"count":4}
+Producing record: alice	{"count":5}
+Producing record: alice	{"count":6}
+Producing record: alice	{"count":7}
+Producing record: alice	{"count":8}
+Producing record: alice	{"count":9}
+Produced record to topic test1 partition [0] @ offset 0
+Produced record to topic test1 partition [0] @ offset 1
+Produced record to topic test1 partition [0] @ offset 2
+Produced record to topic test1 partition [0] @ offset 3
+Produced record to topic test1 partition [0] @ offset 4
+Produced record to topic test1 partition [0] @ offset 5
+Produced record to topic test1 partition [0] @ offset 6
+Produced record to topic test1 partition [0] @ offset 7
+Produced record to topic test1 partition [0] @ offset 8
+Produced record to topic test1 partition [0] @ offset 9
+10 messages were produced to topic test1
+```
+
+## Consume Records
+
+Run the consumer, passing in arguments for (a) whether to produce or consume (consume) (b) the same topic name as used above (c) the local file with configuration parameters to connect to your Confluent Cloud instance and (d, Windows only) a local file with default trusted root CA certificates. Verify that the consumer received all the messages:
+
+```shell
+# Run the producer (Windows)
+$ dotnet run consume test1 %HOMEPATH%/.ccloud/config /path/to/curl/cacert.pem
+
+# Run the producer (other)
+$ dotnet run consume test1 $HOME/.ccloud/config
+```
+
+You should see:
+
+```
+Consumed record with key alice and value {"count":0}, and updated total count to 0
+Consumed record with key alice and value {"count":1}, and updated total count to 1
+Consumed record with key alice and value {"count":2}, and updated total count to 3
+Consumed record with key alice and value {"count":3}, and updated total count to 6
+Consumed record with key alice and value {"count":4}, and updated total count to 10
+Consumed record with key alice and value {"count":5}, and updated total count to 15
+Consumed record with key alice and value {"count":6}, and updated total count to 21
+Consumed record with key alice and value {"count":7}, and updated total count to 28
+Consumed record with key alice and value {"count":8}, and updated total count to 36
+Consumed record with key alice and value {"count":9}, and updated total count to 45
+```
+
+When you are done, press `<ctrl>-c`.
