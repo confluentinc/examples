ea88182ad4fb8d0abdd566fe44f070abf3c6d668
ybyzek <ybyzek@users.noreply.github.com>
2019-05-14 13:11:08 -0400

    DEVX-833: Kinesis -> Confluent Cloud -> cloud storage (#159)

--- utils/helper.sh
+++ utils/helper.sh
@@ -37,6 +37,31 @@ function check_jq() {
   return 0
 }
 
+function check_aws() {
+  if [[ $(type aws 2>&1) =~ "not found" ]]; then
+    echo "AWS CLI is not found. Install AWS CLI and try again"
+    exit 1
+  fi
+
+  return 0
+}
+
+function check_gsutil() {
+  if [[ $(type gsutil 2>&1) =~ "not found" ]]; then
+    echo "Google Cloud gsutil is not found. Install Google Cloud gsutil and try again"
+    exit 1
+  fi
+
+  return 0
+}
+
+function check_gcp_creds() {
+  if [[ -z "$GOOGLE_APPLICATION_CREDENTIALS" ]] && [[ ! -f $HOME/.config/gcloud/application_default_credentials.json ]]; then
+    echo "To run this demo to GCS, either set the env parameter 'GOOGLE_APPLICATION_CREDENTIALS' or run 'gcloud auth application-default login', and then try again."
+    exit 1
+  fi
+}
+
 function check_running_cp() {
   expected_version=$1
 
