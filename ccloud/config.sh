fc0a985138c96881e06a3d951044a4109ca704eb
Yeva Byzek <yeva@confluent.io>
2019-05-15 11:20:46 -0400

    By default do not use CCSR or CCKSQL; update gitignore

--- ccloud/config.sh
+++ ccloud/config.sh
@@ -1,7 +1,7 @@
 # Set to true if you have enabled Confluent Cloud Schema Registry
 # and added the appropriate configurations to your $HOME/.ccloud/config file
 # https://docs.confluent.io/current/quickstart/cloud-quickstart.html#step-3-configure-sr-ccloud
-export USE_CONFLUENT_CLOUD_SCHEMA_REGISTRY=true
+export USE_CONFLUENT_CLOUD_SCHEMA_REGISTRY=false
 
 # Set to true if you have enabled Confluent Cloud KSQL
-export USE_CONFLUENT_CLOUD_KSQL=true
+export USE_CONFLUENT_CLOUD_KSQL=false
