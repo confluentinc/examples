73d214bf69bf25a0faf0b8a4538e00ae1b14367d
Yeva Byzek <yeva@confluent.io>
2019-06-03 09:51:07 -0400

    Update ccloud client examples to use ak-tools-ccloud

--- clients/cloud/kafka-connect-datagen/start-docker.sh
+++ clients/cloud/kafka-connect-datagen/start-docker.sh
@@ -17,4 +17,5 @@ sleep 90
 
 ccloud topic create test1
 . ./submit_datagen_orders_config.sh
-docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer.config /tmp/connect-ccloud.delta --max-messages 5'
+#docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer.config /tmp/ak-tools-ccloud.delta --max-messages 5'
+docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG_PROPERTY_FORMAT" --max-messages 5'
--- clients/cloud/ksql-datagen/start-docker.sh
+++ clients/cloud/ksql-datagen/start-docker.sh
@@ -17,4 +17,5 @@ docker-compose up -d connect
 ccloud topic create test1
 docker-compose up -d ksql-datagen
 sleep 15
-docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer.config /tmp/connect-ccloud.delta --max-messages 5'
+#docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer.config /tmp/ak-tools-ccloud.delta --max-messages 5'
+docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG_PROPERTY_FORMAT" --max-messages 5'
