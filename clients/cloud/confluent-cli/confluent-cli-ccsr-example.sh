8e64bbed625313f2b9066d025ebd1c749fa95a86
ybyzek <ybyzek@users.noreply.github.com>
2019-06-27 21:45:21 -0400

    Add double dash to confluent local produce/consume (#186)

--- clients/cloud/confluent-cli/confluent-cli-ccsr-example.sh
+++ clients/cloud/confluent-cli/confluent-cli-ccsr-example.sh
@@ -14,7 +14,8 @@ ccloud topic create $topic_name || true
 # Produce messages
 num_messages=10
 (for i in `seq 1 $num_messages`; do echo "{\"count\":${i}}" ; done) | \
-   confluent local produce $topic_name --cloud \
+   confluent local produce $topic_name -- \
+                                       --cloud \
                                        --value-format avro \
                                        --property value.schema='{"type":"record","name":"myrecord","fields":[{"name":"count","type":"int"}]}' \
                                        --property basic.auth.credentials.source=${BASIC_AUTH_CREDENTIALS_SOURCE} \
@@ -22,7 +23,8 @@ num_messages=10
                                        --property schema.registry.url=${SCHEMA_REGISTRY_URL}
 
 # Consume messages
-confluent local consume $topic_name --cloud \
+confluent local consume $topic_name -- \
+                                    --cloud \
                                     --value-format avro \
                                     --property basic.auth.credentials.source=${BASIC_AUTH_CREDENTIALS_SOURCE} \
                                     --property schema.registry.basic.auth.user.info=${SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO} \
--- clients/cloud/confluent-cli/confluent-cli-example.sh
+++ clients/cloud/confluent-cli/confluent-cli-example.sh
@@ -11,11 +11,13 @@ ccloud topic create $topic_name || true
 # Produce messages
 num_messages=10
 (for i in `seq 1 $num_messages`; do echo "alice,{\"count\":${i}}" ; done) | \
-   confluent local produce $topic_name --cloud \
+   confluent local produce $topic_name -- \
+                                       --cloud \
                                        --property parse.key=true \
                                        --property key.separator=,
 
 # Consume messages
-confluent local consume $topic_name --cloud \
+confluent local consume $topic_name -- \
+                                    --cloud \
                                     --property print.key=true \
                                     --from-beginning
