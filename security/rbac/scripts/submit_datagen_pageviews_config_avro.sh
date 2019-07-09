0b0411a6087bd4c60764c8aa347436d60c6df821
Yeva Byzek <yeva@confluent.io>
2019-07-09 13:26:06 -0400

    Address comments from Chris

--- security/rbac/scripts/submit_datagen_pageviews_config_avro.sh
+++ security/rbac/scripts/submit_datagen_pageviews_config_avro.sh
@@ -20,8 +20,6 @@ DATA=$( cat << EOF
     "value.converter.basic.auth.credentials.source": "USER_INFO",
     "value.converter.basic.auth.user.info": "client:client1",
     "max.interval": 100,
-    "producer.override.sasl.mechanism": "OAUTHBEARER",
-    "producer.override.sasl.login.callback.handler.class": "io.confluent.kafka.clients.plugins.auth.token.TokenUserLoginCallbackHandler",
     "producer.override.sasl.jaas.config": "org.apache.kafka.common.security.oauthbearer.OAuthBearerLoginModule required username=\"connector\" password=\"connector1\" metadataServerUrls=\"http://localhost:8090\";",
     "tasks.max": "1"
   }
