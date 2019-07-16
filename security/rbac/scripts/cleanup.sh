c3269975e48de818a0657558a47311c1e80e9c8f
Yeva Byzek <yeva@confluent.io>
2019-07-15 22:07:44 -0400

    Check for Confluent CLI in cleanup script

--- security/rbac/scripts/cleanup.sh
+++ security/rbac/scripts/cleanup.sh
@@ -4,6 +4,13 @@
 # Cleanup
 ##################################################
 
+# Source library
+. ../../../utils/helper.sh
+
+check_env || exit 1
+check_cli_v2 || exit 1
+
+
 echo -e "\n# Cleanup"
 rm /tmp/tokenKeyPair.pem
 rm /tmp/tokenPublicKey.pem
