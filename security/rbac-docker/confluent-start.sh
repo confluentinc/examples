78fa143ee0a63cec485c2703f1877a08118983f0
Stanislav Vodetskyi <stan@confluent.io>
2019-07-17 13:22:20 -0700

    removed generated keys

--- security/rbac-docker/confluent-start.sh
+++ security/rbac-docker/confluent-start.sh
@@ -8,6 +8,7 @@ fi
 
 # Generating public and private keys for token signing
 echo "Generating public and private keys for token signing"
+mkdir -p ./conf
 openssl genrsa -out ./conf/keypair.pem 2048
 openssl rsa -in ./conf/keypair.pem -outform PEM -pubout -out ./conf/public.pem
 
