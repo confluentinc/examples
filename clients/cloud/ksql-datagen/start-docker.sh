4c17dcba6b86d1c5e8eb62092a5bb7ed4a4b43ae
Yeva Byzek <yeva@confluent.io>
2019-07-19 14:43:55 -0400

    Delete topics from Docker

--- clients/cloud/ksql-datagen/start-docker.sh
+++ clients/cloud/ksql-datagen/start-docker.sh
@@ -16,5 +16,6 @@ sleep 5
 docker-compose exec connect bash -c 'kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" /tmp/ak-tools-ccloud.delta | tail -1` --command-config /tmp/ak-tools-ccloud.delta --topic test1 --create --replication-factor 3 --partitions 6'
 docker-compose up -d ksql-datagen
 sleep 15
+
 #docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer.config /tmp/ak-tools-ccloud.delta --max-messages 5'
 docker-compose exec connect bash -c 'kafka-console-consumer --topic test1 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer-property ssl.endpoint.identification.algorithm=https --consumer-property sasl.mechanism=PLAIN --consumer-property security.protocol=SASL_SSL --consumer-property sasl.jaas.config="$SASL_JAAS_CONFIG_PROPERTY_FORMAT" --max-messages 5'
