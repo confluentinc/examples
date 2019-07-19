d607095a5c49a08e82d9f7b92083230e4aa1dff1
Yeva Byzek <yeva@confluent.io>
2019-07-19 18:28:14 -0400

    Checkpoint

--- clients/cloud/ksql-datagen/start-docker-avro.sh
+++ clients/cloud/ksql-datagen/start-docker-avro.sh
@@ -17,6 +17,7 @@ sleep 5
 
 docker-compose exec connect bash -c 'kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" /tmp/ak-tools-ccloud.delta | tail -1` --command-config /tmp/ak-tools-ccloud.delta --topic test2 --create --replication-factor 3 --partitions 6'
 docker-compose up -d ksql-datagen-avro
+echo "Sleeping 15 seconds"
 sleep 15
 
 #docker-compose exec connect bash -c 'kafka-avro-console-consumer --topic test2 --bootstrap-server $CONNECT_BOOTSTRAP_SERVERS --consumer.config /tmp/ak-tools-ccloud.delta --property basic.auth.credentials.source=$CONNECT_VALUE_CONVERTER_BASIC_AUTH_CREDENTIALS_SOURCE --property schema.registry.basic.auth.user.info=$CONNECT_VALUE_CONVERTER_SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO --property schema.registry.url=$CONNECT_VALUE_CONVERTER_SCHEMA_REGISTRY_URL --max-messages 5'
