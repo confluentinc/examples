afa55e4124d99cf3380337d0d1fde24b844ef808
Yeva Byzek <yeva@confluent.io>
2019-07-19 14:43:55 -0400

    Delete topics from Docker

--- clients/cloud/ksql-datagen/stop-docker.sh
+++ clients/cloud/ksql-datagen/stop-docker.sh
@@ -9,10 +9,20 @@ check_ccloud_config $CONFIG_FILE || exit
 ../../../ccloud/ccloud-generate-cp-configs.sh $HOME/.ccloud/config
 source ./delta_configs/env.delta
 
+topics_to_delete="test1 test2"
+topics=$(docker-compose exec connect bash -c 'kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" /tmp/ak-tools-ccloud.delta | tail -1` --command-config /tmp/ak-tools-ccloud.delta --list 2>/dev/null')
+for topic in $topics_to_delete
+do
+  echo $topics | grep $topic &>/dev/null
+  if [[ $? == 0 ]]; then
+    docker-compose exec connect bash -c 'kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" /tmp/ak-tools-ccloud.delta | tail -1` --command-config /tmp/ak-tools-ccloud.delta --topic $topic --delete'
+  fi
+done
+
 docker-compose down
 
 for v in $(docker volume ls -q --filter="dangling=true"); do
-        docker volume rm "$v"
+  docker volume rm "$v"
 done
 
 # Delete subjects from Confluent Cloud Schema Registry
@@ -22,12 +32,3 @@ do
   curl -X DELETE --silent -u $SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO $SCHEMA_REGISTRY_URL/subjects/$subject
 done
 
-topics_to_delete="test1 test2"
-topics=$(kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" ~/.ccloud/config | tail -1` --command-config ~/.ccloud/config --list)
-for topic in $topics_to_delete
-do
-  echo $topics | grep $topic &>/dev/null
-  if [[ $? == 0 ]]; then
-    kafka-topics --bootstrap-server `grep "^\s*bootstrap.server" ~/.ccloud/config | tail -1` --command-config ~/.ccloud/config --delete --topic $topic
-  fi
-done
