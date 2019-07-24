8b38338ea2e811f026fdfc09e2b926ad59bee5df
Stanislav Vodetskyi <stan@confluent.io>
2019-07-24 15:46:24 -0700

    allow prefix to be configured

--- security/rbac-docker/pull-docker-images.sh
+++ security/rbac-docker/pull-docker-images.sh
@@ -2,7 +2,13 @@
 
 TAG=$1
 `aws ecr get-login --no-include-email --region us-west-2`
-PREFIX=368821881613.dkr.ecr.us-west-2.amazonaws.com/confluentinc
+
+if [ -z "$2" ]; then
+    PREFIX=368821881613.dkr.ecr.us-west-2.amazonaws.com/confluentinc
+else
+    PREFIX=$2
+fi
+
 declare -a NAMES=("cp-zookeeper" "cp-enterprise-kafka" "cp-schema-registry" \
                   "cp-enterprise-control-center" "cp-enterprise-replicator" \
                   "cp-ksql-server" "cp-ksql-cli" "cp-kafka-connect" "cp-kafka-rest" \
