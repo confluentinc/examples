58e85a256a1de6835debfd942f031a14602a2763
Yeva Byzek <yeva@confluent.io>
2019-09-26 17:53:14 -0400

    Removing troubleshooting section; using Jennifer's start config

--- multiregion/scripts/start.sh
+++ multiregion/scripts/start.sh
@@ -4,10 +4,12 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ${DIR}/stop.sh
 
-echo "Sleeping 2 seconds"
-sleep 2
-
 docker-compose up -d
+
+echo "Sleeping 30 seconds"
+sleep 30
+
+${DIR}/latency_docker.sh
 
 echo "Sleeping 30 seconds"
 sleep 30
@@ -19,16 +21,8 @@ sleep 10
 
 ${DIR}/describe-topics.sh
 
-echo "Sleeping 2 seconds"
-sleep 2
-
-${DIR}/latency_docker.sh
-
-echo "Sleeping 30 seconds"
-sleep 30
-
-# Describe topics again
-${DIR}/describe-topics.sh
+echo "Sleeping 10 seconds"
+sleep 10
 
 ${DIR}/start-producer.sh
 
