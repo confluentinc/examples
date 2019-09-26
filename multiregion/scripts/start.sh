20fca398fdb4a2962696fa5170f15541529dfb8d
Yeva Byzek <yeva@confluent.io>
2019-09-26 17:43:22 -0400

    DEVX-1118: Troubleshoot Leader showing none

--- multiregion/scripts/start.sh
+++ multiregion/scripts/start.sh
@@ -4,6 +4,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
 ${DIR}/stop.sh
 
+echo "Sleeping 2 seconds"
+sleep 2
+
 docker-compose up -d
 
 echo "Sleeping 30 seconds"
@@ -11,11 +14,20 @@ sleep 30
 
 ${DIR}/create-topics.sh
 
-${DIR}/latency_docker.sh
-
 echo "Sleeping 10 seconds"
 sleep 10
 
+${DIR}/describe-topics.sh
+
+echo "Sleeping 2 seconds"
+sleep 2
+
+${DIR}/latency_docker.sh
+
+echo "Sleeping 30 seconds"
+sleep 30
+
+# Describe topics again
 ${DIR}/describe-topics.sh
 
 ${DIR}/start-producer.sh
@@ -24,4 +36,3 @@ echo "Sleeping 30 seconds"
 sleep 30
 
 ${DIR}/start-consumer.sh
-
