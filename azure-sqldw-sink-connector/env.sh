d8f95b7463ad6f8d6d72365d27f5ef6a7f65e334
hubert dulay <hubert.dulay@gmail.com>
2019-10-12 11:44:51 -0400

    removing sqldw artifacts

--- azure-sqldw-sink-connector/env.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-export ADMIN_USER=hubert
-export ADMIN_PASS=confluent10!
-export IP_ADDRESS=69.206.183.82
-export RESOURCE_GROUP=quickstartResourceGroup
-export SERVER=hubert-sql-server
-export DWNAME=quickstartDataWarehouse
-export SCHEMA=$(cat clickstream_users_schema.avro )
-export REGION=eastus
