d868451232245559afa49145555ada8e47849062
ybyzek <ybyzek@users.noreply.github.com>
2019-10-31 12:37:31 -0400

    DEVX-1245: UX improvements to Schema translation demo (#325)

--- replicator-schema-translation/scripts/set_sr_modes_pre_translation.sh
+++ replicator-schema-translation/scripts/set_sr_modes_pre_translation.sh
@@ -2,6 +2,8 @@
 
 HEADER="Content-Type: application/json"
 
+echo -e "\nSetting srcSchemaregistry to READONLY mode:"
 curl -X PUT -H "${HEADER}" --data '{"mode": "READONLY"}' http://srcSchemaregistry:8085/mode
-curl -X PUT -H "${HEADER}" --data '{"mode": "IMPORT"}' http://destSchemaregistry:8086/mode
 
+echo -e "\nSetting destSchemaregistry to IMPORT mode:"
+curl -X PUT -H "${HEADER}" --data '{"mode": "IMPORT"}' http://destSchemaregistry:8086/mode
