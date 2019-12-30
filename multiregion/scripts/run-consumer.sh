d657adafc39f9cfe608459fdaa039d5fc3adf7a0
AdheipSingh <34169002+AdheipSingh@users.noreply.github.com>
2019-12-30 15:46:46 +0530

    Update consumer
    Consumer config missing, added it for west region.

--- multiregion/scripts/run-consumer.sh
+++ multiregion/scripts/run-consumer.sh
@@ -7,6 +7,8 @@ docker-compose exec broker-east-3 kafka-consumer-perf-test --topic multi-region-
     --threads 1 \
     --broker-list broker-west-1:19091,broker-east-3:19093 \
     --timeout 20000
+    --consumer.config /etc/kafka/demo/consumer-west.config
+
 
 echo -e "\n\n==> Consume from east: Multi-region Async Replication reading from Observer in east (topic: multi-region-async) \n"
 
