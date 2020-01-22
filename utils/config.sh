f8a87f2aa41e7fd464a80a736e7e8fe372628081
Yeva Byzek <yeva@confluent.io>
2020-01-22 13:49:45 -0500

    Remove export

--- utils/config.sh
+++ utils/config.sh
@@ -1,20 +1,20 @@
 #####################################################
 # File with version configuration parameters
 #####################################################
-# Example for GA release
-# export CP_VERSION_MAJOR=5.4
-# export CP_VERSION_FULL=5.4.0
-# export GH_BRANCH=5.4.0-post
-# export JAR_VERSION=5.4.0
+# Example for GA release:
+#   CP_VERSION_MAJOR=5.4
+#   CP_VERSION_FULL=5.4.0
+#   GH_BRANCH=5.4.0-post
+#   JAR_VERSION=5.4.0
 #
-# Example for pre-GA release
-# export CP_VERSION_MAJOR=5.4
-# export CP_VERSION_FULL=5.4.1
-# export GH_BRANCH=5.4.x
-# export JAR_VERSION=5.4.1-SNAPSHOT
+# Example for pre-GA release:
+#   CP_VERSION_MAJOR=5.4
+#   CP_VERSION_FULL=5.4.1
+#   GH_BRANCH=5.4.x
+#   JAR_VERSION=5.4.1-SNAPSHOT
 #####################################################
 
-export CP_VERSION_MAJOR=5.4
-export CP_VERSION_FULL=5.4.0
-export GH_BRANCH=5.4.0-post
-export JAR_VERSION=5.4.0
+CP_VERSION_MAJOR=5.4
+CP_VERSION_FULL=5.4.0
+GH_BRANCH=5.4.0-post
+JAR_VERSION=5.4.0
