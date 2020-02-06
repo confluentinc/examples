807dffe3e6d638fd51a9d4df38c641d088b150ad
Yeva Byzek <yeva@confluent.io>
2020-02-06 10:32:00 -0500

    Check response of each KSQL query

--- kinesis-cloud/start.sh
+++ kinesis-cloud/start.sh
@@ -89,16 +89,22 @@ echo -e "\nSubmit KSQL queries\n"
 properties='"ksql.streams.auto.offset.reset":"earliest","ksql.streams.cache.max.bytes.buffering":"0"'
 while read ksqlCmd; do
   echo -e "\n$ksqlCmd\n"
-  curl -X POST $KSQL_ENDPOINT/ksql \
+  response=$(curl -X POST $KSQL_ENDPOINT/ksql \
        -H "Content-Type: application/vnd.ksql.v1+json; charset=utf-8" \
        -u $KSQL_BASIC_AUTH_USER_INFO \
+       --silent \
        -d @<(cat <<EOF
 {
   "ksql": "$ksqlCmd",
   "streamsProperties": {$properties}
 }
 EOF
-)
+))
+  echo $response
+  if [[ ! "$response" =~ "SUCCESS" ]]; then
+    echo -e "\nWARNING: KSQL command '$ksqlCmd' did not include \"SUCCESS\" in the response.  Please troubleshoot."
+    sleep 2
+  fi
 done <ksql.commands
 echo -e "\nSleeping 20 seconds after submitting KSQL queries\n"
 sleep 20
