1ee5a2d61c29f4115f8d96c131999f02392beba1
Yeva Byzek <yeva@confluent.io>
2020-02-06 09:55:55 -0500

    Cleanup outputs

--- kinesis-cloud/start.sh
+++ kinesis-cloud/start.sh
@@ -45,11 +45,15 @@ if [[ $? != 0 ]]; then
 fi
 echo -e "\nSleeping 60 seconds waiting for Kinesis stream to be created\n"
 sleep 60
-aws kinesis describe-stream --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION
+#aws kinesis describe-stream --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION
 # File has ~500 records, so run several times to fulfill the flush size requirement of 1000 records / partition for the sink connectors
-echo -e "\nWriting ~11k records to Kinesis\n"
+echo -e "Writing ~11k records to Kinesis\n"
 for i in {1..22}; do
   aws kinesis put-records --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION --records file://../utils/table.locations.cloud.json >/dev/null
+  if [[ $? != 0 ]]; then
+    echo "ERROR: Received a non-zero exit code when trying to put-records into the AWS Kinesis stream. Please troubleshoot"
+    exit $?
+  fi
 done
 echo -e "\nSleeping 10 seconds\n"
 sleep 10
