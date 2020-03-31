6a60946dd392515ef8156202dcd63f71190faf1f
Yeva Byzek <ybyzek@users.noreply.github.com>
2020-03-31 15:28:56 -0400

    DEVX-1499: Validate confluentinc/examples/music demo end-to-end with 5.5 image (#528)

--- microservices-orders/services-start.sh
+++ microservices-orders/services-start.sh
@@ -4,7 +4,7 @@
 . ../utils/config.env
 
 RESTPORT=18894
-JAR=/usr/share/java/kafka-streams-examples/kafka-streams-examples-${JAR_VERSION}-standalone.jar
+JAR=/usr/share/java/kafka-streams-examples/kafka-streams-examples-${CONFLUENT}-standalone.jar
 
 java -cp $JAR io.confluent.examples.streams.microservices.OrdersService broker:9092 http://schema-registry:8081 localhost $RESTPORT > /dev/null 2>&1 &
 
