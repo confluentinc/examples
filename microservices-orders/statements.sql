2c1aaaa13ddf56a0cf36f2376bf62bc11f875797
Yeva Byzek <yeva@confluent.io>
2020-04-07 16:16:37 -0400

    DEVX-1656: update statements.sql for microservices-orders

--- microservices-orders/statements.sql
+++ microservices-orders/statements.sql
@@ -1,15 +1,14 @@
---Identify fields in schema, even with Avro, because this is a script (https://github.com/confluentinc/ksql/issues/1031)
-CREATE STREAM orders (id varchar, customerid bigint, state varchar, product varchar, quantity int, price double) WITH (kafka_topic='orders', value_format='AVRO');
+CREATE STREAM orders WITH (kafka_topic='orders', value_format='AVRO');
 
 --Next 3 steps are required to create a TABLE with keys of String type
 
---1. Identify fields in schema, even with Avro, because this is a script (https://github.com/confluentinc/ksql/issues/1031)
-CREATE STREAM customers_with_wrong_format_key (id bigint, firstname varchar, lastname varchar, email varchar, address varchar, level varchar) WITH (kafka_topic='customers', value_format='AVRO');
+--1. Create a stream
+CREATE STREAM customers_with_wrong_format_key WITH (kafka_topic='customers', value_format='AVRO');
 
 --2. Derive a new stream with the required key changes.
 --The CAST statement converts the key to the required format.
 --The PARTITION BY clause re-partitions the stream based on the new, converted key.
-CREATE STREAM customers_with_proper_key WITH (KAFKA_TOPIC='customers-with-proper-key', PARTITIONS=1) AS SELECT CAST(id as BIGINT) as customerid, firstname, lastname, email, address, level FROM customers_with_wrong_format_key PARTITION BY CAST(id as BIGINT);
+CREATE STREAM customers_with_proper_key WITH (KAFKA_TOPIC='customers-with-proper-key') AS SELECT CAST(id as BIGINT) as customerid, firstname, lastname, email, address, level FROM customers_with_wrong_format_key PARTITION BY CAST(id as BIGINT);
 
 --3. Create the table on the properly keyed stream
 CREATE TABLE customers_table (rowkey bigint KEY, customerid bigint, firstname varchar, lastname varchar, email varchar, address varchar, level varchar) WITH (KAFKA_TOPIC='customers-with-proper-key', VALUE_FORMAT='AVRO', KEY='customerid');
