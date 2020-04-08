8004a7cf3ec6d5001bedded45187c7bbc91005a0
Bill Bejeck <bill@confluent.io>
2020-04-08 13:27:14 -0400

    Apply suggestions from code review
    Co-Authored-By: Yeva Byzek <ybyzek@users.noreply.github.com>

--- clickstream/start.sh
+++ clickstream/start.sh
@@ -52,13 +52,13 @@ for i in {1..100}; do
  
  docker-compose exec ksqldb-server curl -X PUT localhost:8083/connectors/DATAGEN_CLICKSTREAM/pause
 
- echo -e "\nPaused the clickstream generate for 35 seconds\n"
+ echo -e "\nPaused the clickstream datagen for 35 seconds\n"
  sleep 35
 
 
  docker-compose exec ksqldb-server curl -X PUT localhost:8083/connectors/DATAGEN_CLICKSTREAM/resume
 
- echo -e "\Resumed the clickstream generate for 90 seconds. Then the cycle will repeat\n"
+ echo -e "\Resumed the clickstream datagen for 90 seconds. Then the cycle will repeat\n"
  sleep 90
 
 done
