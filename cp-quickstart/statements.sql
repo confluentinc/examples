e5b5e81ce41ec882e56d24d8c296b4ef0f3ce323
Yeva Byzek <yeva@confluent.io>
2020-04-08 07:52:34 -0400

    Update ksqlDB statements

--- cp-quickstart/statements.sql
+++ cp-quickstart/statements.sql
@@ -1,9 +1,9 @@
-CREATE STREAM pageviews_original WITH (kafka_topic='pageviews', value_format='AVRO');
+CREATE STREAM pageviews WITH (kafka_topic='pageviews', value_format='AVRO');
 
-CREATE TABLE users_original WITH (kafka_topic='users', value_format='AVRO', key = 'userid');
+CREATE TABLE users WITH (kafka_topic='users', value_format='AVRO', key = 'userid');
 
-CREATE STREAM pageviews_female AS SELECT users_original.userid AS userid, pageid, regionid, gender FROM pageviews_original LEFT JOIN users_original ON pageviews_original.userid = users_original.userid WHERE gender = 'FEMALE';
+CREATE STREAM pageviews_female AS SELECT users.userid AS userid, pageid, regionid, gender FROM pageviews LEFT JOIN users ON pageviews.userid = users.userid WHERE gender = 'FEMALE';
 
 CREATE STREAM pageviews_female_like_89 WITH (kafka_topic='pageviews_enriched_r8_r9', value_format='AVRO') AS SELECT * FROM pageviews_female WHERE regionid LIKE '%_8' OR regionid LIKE '%_9';
 
-CREATE TABLE pageviews_regions WITH (value_format='AVRO') AS SELECT gender, regionid , COUNT(*) AS numusers FROM pageviews_female WINDOW TUMBLING (size 30 second) GROUP BY gender, regionid HAVING COUNT(*) > 1;
+CREATE TABLE pageviews_regions AS SELECT gender, regionid , COUNT(*) AS numusers FROM pageviews_female WINDOW TUMBLING (size 30 second) GROUP BY gender, regionid HAVING COUNT(*) > 1;
