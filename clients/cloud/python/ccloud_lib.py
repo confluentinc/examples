c48605a8a4f53c42a0740bbe9c94321ca88867f6
Yeva Byzek <yeva@confluent.io>
2020-04-09 19:14:20 -0400

    Add clarifying comment

--- clients/cloud/python/ccloud_lib.py
+++ clients/cloud/python/ccloud_lib.py
@@ -159,6 +159,7 @@ def create_topic(conf, topic):
             print("Topic {} created".format(topic))
         except Exception as e:
             # Continue if error code TOPIC_ALREADY_EXISTS, which may be true
+            # Otherwise fail fast
             if e.args[0].code() != KafkaError.TOPIC_ALREADY_EXISTS:
                 print("Failed to create topic {}: {}".format(topic, e))
                 sys.exit(1)
