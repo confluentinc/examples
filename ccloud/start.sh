4282174306a2c3933520cbec00eaceca6f7bf5e5
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-04-15 11:24:18 -0500

    DEVX-1496 ccloud minor UX improvements

--- ccloud/start.sh
+++ ccloud/start.sh
@@ -27,6 +27,7 @@ export CONFIG_FILE=~/.ccloud/config
 check_ccloud_config $CONFIG_FILE || exit 1
 check_ccloud_version 0.264.0 || exit 1
 check_ccloud_logged_in || exit 1
+printf "Done\n"
 
 if ! check_cp ; then
   echo "This demo uses Confluent Replicator which requires Confluent Platform, however this host is running Confluent Community Software. Exiting"
@@ -36,9 +37,7 @@ printf "\n"
 
 echo ====== Cleaning up previous run
 ./stop.sh
-echo ====== Done with cleanup
-printf "\n"
-
+printf "\nDone with cleanup\n\n"
 
 echo ====== Installing kafka-connect-datagen
 confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:$KAFKA_CONNECT_DATAGEN_VERSION
@@ -61,11 +60,12 @@ echo ====== Set current Confluent Cloud
 # Set Kafka cluster and service account
 ccloud_cli_set_kafka_cluster_use $CLOUD_KEY $CONFIG_FILE || exit 1
 serviceAccount=$(ccloud_cli_get_service_account $CLOUD_KEY $CONFIG_FILE) || exit 1
+printf "\n"
 
 echo ====== Validate Schema Registry credentials
 # Validate credentials to Confluent Cloud Schema Registry
 validate_confluent_cloud_schema_registry $SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO $SCHEMA_REGISTRY_URL || exit 1
-printf "\n"
+printf "Done\n\n"
 
 echo ====== Start local Confluent Control Center to monitor Cloud and local clusters
 # For the KSQL Server backed to Confluent Cloud, set the REST port, instead of the default 8088 which is already in use by the local KSQL server
@@ -98,7 +98,7 @@ printf "\n"
 
 echo ====== Submit datagen connector for pageviews 
 . ./connectors/submit_datagen_pageviews_config.sh
-printf "\n"
+printf "\n\n"
 
 echo ====== Start Local Connect cluster that connects to CCloud Kafka
 mkdir -p $CONFLUENT_CURRENT/connect
@@ -117,12 +117,11 @@ connect-distributed $CONNECT_CONFIG > $CONFLUENT_CURRENT/connect/connect-ccloud.
 MAX_WAIT=40
 echo "Waiting up to $MAX_WAIT seconds for the connect worker that connects to Confluent Cloud to start"
 retry $MAX_WAIT check_connect_up_logFile $CONFLUENT_CURRENT/connect/connect-ccloud.stdout || exit 1
-printf "\n"
+printf "\n\n"
 
-echo ====== Create topic users in CCloud cluster
+echo ====== Create topic users and set ACLs in CCloud cluster
 ccloud kafka topic create users
 ccloud kafka acl create --allow --service-account $serviceAccount --operation WRITE --topic users
-. ./connectors/submit_datagen_users_config.sh
 printf "\n"
 
 echo ====== submit Datagen connector for users
@@ -136,11 +135,11 @@ printf "\n"
 
 echo ====== Starting Replicator and sleeping 60 seconds
 . ./connectors/submit_replicator_config.sh
-printf "\n"
 sleep 60
+printf "\n"
 
 echo ====== Creating Confluent Cloud KSQL application
 ./create_ksql_app.sh || exit 1
 printf "\n"
 
-echo -e "\nDONE! Connect to your Confluent Cloud UI or Confluent Control Center at http://localhost:9021\n"
+printf "\nDONE! Connect to your Confluent Cloud UI or Confluent Control Center at http://localhost:9021\n"
