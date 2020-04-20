9707c43bbdc49ca62d346459202d6a6e011a5ba8
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-04-20 14:00:15 -0500

    DEVX-1579 parameterizes ccloud/beginner demo cloud provider and region

--- ccloud/beginner-cloud/start.sh
+++ ccloud/beginner-cloud/start.sh
@@ -89,10 +89,12 @@ ccloud environment use $ENVIRONMENT
 # Create a new Kafka cluster and specify it as the default
 ##################################################
 
-CLUSTER_NAME="demo-kafka-cluster"
+CLUSTER_NAME="${CLUSTER_NAME:-demo-kafka-cluster}"
+CLUSTER_CLOUD="${CLUSTER_CLOUD:-gcp}"
+CLUSTER_REGION="${CLUSTER_REGION:-us-central1}"
 echo -e "\n# Create and specify active Kafka cluster"
-echo "ccloud kafka cluster create $CLUSTER_NAME --cloud gcp --region us-central1"
-OUTPUT=$(ccloud kafka cluster create $CLUSTER_NAME --cloud gcp --region us-central1)
+echo "ccloud kafka cluster create $CLUSTER_NAME --cloud $CLUSTER_CLOUD --region $CLUSTER_REGION"
+OUTPUT=$(ccloud kafka cluster create $CLUSTER_NAME --cloud $CLUSTER_CLOUD --region $CLUSTER_REGION)
 status=$?
 echo "$OUTPUT"
 if [[ $status != 0 ]]; then
@@ -128,8 +130,7 @@ echo "ccloud api-key use $API_KEY --resource $CLUSTER"
 ccloud api-key use $API_KEY --resource $CLUSTER
 
 echo -e "\n# Wait 120 seconds for the user credentials to propagate"
-sleep 120
-
+sleep 240
 
 ##################################################
 # Produce and consume with Confluent Cloud CLI
