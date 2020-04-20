97163414688952f2a8ed108cc0333f80e5552c86
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-04-20 11:56:16 -0500

    DEVX-1579 Updates all datagen examples to latest available (0.3.1) for 5.4.1

--- security/rbac/scripts/enable-rbac-connect.sh
+++ security/rbac/scripts/enable-rbac-connect.sh
@@ -73,8 +73,8 @@ echo "confluent iam rolebinding list --principal User:$USER_ADMIN_CONNECT --kafk
 confluent iam rolebinding list --principal User:$USER_ADMIN_CONNECT --kafka-cluster-id $KAFKA_CLUSTER_ID
 
 echo -e "\n# Install kafka-connect-datagen"
-echo "confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:0.2.0"
-confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:0.2.0
+echo "confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:0.3.1"
+confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:0.3.1
 
 echo -e "\n# Bring up Connect"
 confluent local start connect
