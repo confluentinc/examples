6afe7ccb72c9c26132f6676e9459fb6bfad92c49
Yeva Byzek <yeva@confluent.io>
2020-04-25 14:00:25 -0400

    Add ACLs for pageviews and users

--- ccloud/start-docker.sh
+++ ccloud/start-docker.sh
@@ -52,9 +52,11 @@ echo ====== Validate credentials to Confluent Cloud Schema Registry
 validate_confluent_cloud_schema_registry $SCHEMA_REGISTRY_BASIC_AUTH_USER_INFO $SCHEMA_REGISTRY_URL || exit 1
 printf "Done\n\n"
 
-echo ====== Creating cloud topics
+echo ====== Creating cloud topics users and pageviews and setting ACLs
 ccloud kafka topic create users
 ccloud kafka topic create pageviews
+ccloud kafka acl create --allow --service-account $serviceAccount --operation WRITE --topic pageviews
+ccloud kafka acl create --allow --service-account $serviceAccount --operation WRITE --topic users
 printf "\n"
 
 echo ====== Starting local services in Docker
