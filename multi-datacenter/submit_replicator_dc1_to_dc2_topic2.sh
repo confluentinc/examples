6c87220f36e9e825532c17fde61365c77b4d195f
Yeva Byzek <yeva@confluent.io>
2020-04-27 22:02:33 -0400

    Make same change for multi-datacenter

--- multi-datacenter/submit_replicator_dc1_to_dc2_topic2.sh
+++ multi-datacenter/submit_replicator_dc1_to_dc2_topic2.sh
@@ -14,6 +14,7 @@ DATA=$( cat << EOF
     "src.consumer.group.id": "replicator-dc1-to-dc2-topic2",
     "src.consumer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringConsumerInterceptor",
     "src.consumer.confluent.monitoring.interceptor.bootstrap.servers": "broker-dc2:29092",
+    "src.kafka.timestamps.topic.replication.factor": 1,
     "src.kafka.timestamps.producer.interceptor.classes": "io.confluent.monitoring.clients.interceptor.MonitoringProducerInterceptor",
     "src.kafka.timestamps.producer.confluent.monitoring.interceptor.bootstrap.servers": "broker-dc2:29092",
     "dest.kafka.bootstrap.servers": "broker-dc2:29092",
