a326450dcb19a0a633f708d6b40090713738e884
Yeva Byzek <yeva@confluent.io>
2020-05-06 18:36:59 -0400

    Optimize boolean check

--- cloud-etl/create_kinesis_streams.sh
+++ cloud-etl/create_kinesis_streams.sh
@@ -14,15 +14,15 @@ if [[ $? != 0 ]]; then
 fi
 echo -e "\nSleeping 60 seconds waiting for Kinesis stream to be created\n"
 sleep 60
+
+if [[ $(get_aws_cli_version) -eq 2 ]]; then
+  V2_OPTS="--cli-binary-format raw-in-base64-out"
+fi
 #aws kinesis describe-stream --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION
 # File has ~500 records, so run several times to fulfill the flush size requirement of 1000 records / partition for the sink connectors
 echo -e "Writing ~11k records to Kinesis\n"
 for i in {1..22}; do
-  if [[ get_aws_cli_verison -eq 2 ]]; then
-    aws kinesis put-records --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION --records file://./eventLogs.json --profile $AWS_PROFILE --cli-binary-format raw-in-base64-out >/dev/null
-  else
-    aws kinesis put-records --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION --records file://./eventLogs.json --profile $AWS_PROFILE >/dev/null
-  fi
+  aws kinesis put-records --stream-name $KINESIS_STREAM_NAME --region $KINESIS_REGION --records file://./eventLogs.json --profile $AWS_PROFILE $V2_OPTS >/dev/null
   if [[ $? != 0 ]]; then
     echo "ERROR: Received a non-zero exit code when trying to put-records into the AWS Kinesis stream. Please troubleshoot"
     exit 1
