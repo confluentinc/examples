2e1536dc0d54e178734e61339a80cb13f7f658c0
Yeva Byzek <yeva@confluent.io>
2020-05-11 21:13:19 -0400

    Move Docker compose to top

--- ccloud/stop-docker.sh
+++ ccloud/stop-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+docker-compose down -v
+
 if [ -z "$1" ]; then
   echo "ERROR: Must supply argument that is the client configuration file created from './start-docker.sh'. (Is it in stack-configs/ folder?) "
   exit 1
@@ -7,4 +9,3 @@ else
   ccloud-stack/ccloud_stack_destroy.sh $CONFIG_FILE
 fi
 
-docker-compose down -v
