f5766cf41d2ae85bb70f16df6d26b58e562caa65
Yeva Byzek <yeva@confluent.io>
2020-05-27 14:31:47 -0400

    Add timestamp

--- cloud-etl/create_rds.sh
+++ cloud-etl/create_rds.sh
@@ -14,7 +14,7 @@ check_psql \
   || exit 1
 
 check_aws_rds_db_ready() {
-  STATUS=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER | jq -r ".DBInstances[0].DBInstanceStatus")
+  STATUS=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER --profile $AWS_PROFILE | jq -r ".DBInstances[0].DBInstanceStatus")
   if [[ "$STATUS" == "available" ]]; then
     return 0
   fi
@@ -43,26 +43,27 @@ echo "Waiting up to $MAX_WAIT seconds for AWS RDS PostgreSQL database $DB_INSTAN
 retry $MAX_WAIT check_aws_rds_db_ready $DB_INSTANCE_IDENTIFIER || exit 1
 print_pass "Database $DB_INSTANCE_IDENTIFIER is available"
 
-SECURITY_GROUP=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER | jq -r ".DBInstances[0].VpcSecurityGroups[0].VpcSecurityGroupId")
+SECURITY_GROUP=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER --profile $AWS_PROFILE | jq -r ".DBInstances[0].VpcSecurityGroups[0].VpcSecurityGroupId")
 aws ec2 authorize-security-group-ingress --group-id $SECURITY_GROUP --cidr 0.0.0.0/0 --protocol all
 aws ec2 authorize-security-group-egress --group-id $SECURITY_GROUP --cidr 0.0.0.0/0 --protocol all
 
 echo "Creating eventLogs.sql"
 rm -fr eventLogs.sql
+timestamp="1590510745323"
 for row in $(jq -r '.[] .Data' eventLogs.json); do
   read -r eventSourceIP eventAction result eventDuration <<<"$(echo "$row" | jq -r '"\(.eventSourceIP) \(.eventAction) \(.result) \(.eventDuration)"')"
-  echo -e "$eventSourceIP\t$eventAction\t$result\t$eventDuration" >> eventLogs.sql
+  echo -e "$timestamp\t$eventSourceIP\t$eventAction\t$result\t$eventDuration" >> eventLogs.sql
 done
 
 echo "Create table eventLogs"
-export CONNECTION_HOST=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER | jq -r ".DBInstances[0].Endpoint.Address")
-export CONNECTION_PORT=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER | jq -r ".DBInstances[0].Endpoint.Port")
+export CONNECTION_HOST=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER --profile $AWS_PROFILE | jq -r ".DBInstances[0].Endpoint.Address")
+export CONNECTION_PORT=$(aws rds describe-db-instances --db-instance-identifier $DB_INSTANCE_IDENTIFIER --profile $AWS_PROFILE | jq -r ".DBInstances[0].Endpoint.Port")
 PGPASSWORD=pg12345678 psql \
    --host $CONNECTION_HOST \
    --port $CONNECTION_PORT \
    --username pg \
    --dbname $DB_INSTANCE_IDENTIFIER \
-   --command "CREATE TABLE eventLogs (eventSourceIP VARCHAR(255), eventAction VARCHAR(255), result VARCHAR(255), eventDuration BIGINT);"
+   --command "CREATE TABLE eventLogs (timestamp VARCAR(255), eventSourceIP VARCHAR(255), eventAction VARCHAR(255), result VARCHAR(255), eventDuration BIGINT);"
 echo "Populate table eventLogs"
 PGPASSWORD=pg12345678 psql \
    --host $CONNECTION_HOST \
