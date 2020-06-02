82797a8c2d5d752ae996bb184afc1150be25aa71
Gianluca Natali <8244206+gianlucanatali@users.noreply.github.com>
2020-06-02 17:10:00 +0200

    adding ccloud::create_ccloud_stack_sub
    extracting logic from  ccloud::create_ccloud_stack into reusable function for my demo (ccloud::create_ccloud_stack_sub)

--- utils/ccloud_library.sh
+++ utils/ccloud_library.sh
@@ -737,6 +737,16 @@ function ccloud::create_ccloud_stack() {
   CLUSTER_NAME=demo-kafka-cluster-$SERVICE_ACCOUNT_ID
   CLUSTER_CLOUD="${CLUSTER_CLOUD:-aws}"
   CLUSTER_REGION="${CLUSTER_REGION:-us-west-2}"
+  KSQL_NAME="demo-ksql-$SERVICE_ACCOUNT_ID"
+
+  mkdir -p stack-configs
+  CLIENT_CONFIG="stack-configs/java-service-account-$SERVICE_ACCOUNT_ID.config"
+
+  ccloud::create_ccloud_stack_sub
+}
+
+function ccloud::create_ccloud_stack_sub() {
+  
   CLUSTER=$(ccloud::create_and_use_cluster $CLUSTER_NAME $CLUSTER_CLOUD $CLUSTER_REGION)
   if [[ "$CLUSTER" == "" ]] ; then
     echo "Kafka cluster id is empty"
@@ -759,7 +769,6 @@ function ccloud::create_ccloud_stack() {
   SCHEMA_REGISTRY_CREDS=$(ccloud::create_credentials_resource $SERVICE_ACCOUNT_ID $SCHEMA_REGISTRY)
 
   if $enable_ksql ; then
-    KSQL_NAME="demo-ksql-$SERVICE_ACCOUNT_ID"
     KSQL=$(ccloud::create_ksql_app $KSQL_NAME $CLUSTER)
     KSQL_ENDPOINT=$(ccloud ksql app describe $KSQL -o json | jq -r ".endpoint")
     KSQL_CREDS=$(ccloud::create_credentials_resource $SERVICE_ACCOUNT_ID $KSQL)
@@ -768,8 +777,7 @@ function ccloud::create_ccloud_stack() {
 
   ccloud::create_acls_all_resources_full_access $SERVICE_ACCOUNT_ID
 
-  mkdir -p stack-configs
-  CLIENT_CONFIG="stack-configs/java-service-account-$SERVICE_ACCOUNT_ID.config"
+  
   cat <<EOF > $CLIENT_CONFIG
 # ------------------------------
 # Confluent Cloud connection information for demo purposes only
