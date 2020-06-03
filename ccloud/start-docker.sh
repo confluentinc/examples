a6908ab3874496ba87a702a1897b1ffdb0edd6c7
Yeva Byzek <yeva@confluent.io>
2020-06-02 21:56:44 -0400

    Create topic pageviews directly from start script; remove kafka-create-topics

--- ccloud/start-docker.sh
+++ ccloud/start-docker.sh
@@ -68,18 +68,20 @@ echo ====== Starting local services in Docker
 docker-compose up -d
 printf "\n"
 
-# Verify topics exist
-MAX_WAIT=240
-echo "Waiting up to $MAX_WAIT seconds for topic pageviews to exist in local cluster"
-retry $MAX_WAIT check_topic_exists kafka kafka:9092 pageviews || exit 1
-echo "Topic pageviews exists in local cluster!"
-
 MAX_WAIT=240
 echo "Waiting up to $MAX_WAIT seconds for connect-local to start"
 retry $MAX_WAIT check_connect_up connect-local || exit 1
 echo "Waiting up to $MAX_WAIT seconds for connect-cloud to start"
 retry $MAX_WAIT check_connect_up connect-cloud || exit 1
 printf "\n\n"
+
+echo ====== Create topic pageviews in local cluster
+docker-compose exec kafka kafka-topics --bootstrap-server localhost:9092 --create --topic pageviews --partitions 6 --replication-factor 1
+MAX_WAIT=30
+echo "Waiting up to $MAX_WAIT seconds for topic pageviews to exist in local cluster"
+retry $MAX_WAIT check_topic_exists kafka kafka:9092 pageviews || exit 1
+echo "Topic pageviews exists in local cluster"
+printf "\n"
 
 echo ====== Deploying kafka-connect-datagen for users 
 source ./connectors/submit_datagen_users_config.sh
