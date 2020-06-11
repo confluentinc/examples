3c4ac0e1ff40bb5beabd37809011e8636f288c68
Andy Coates <big-andy-coates@users.noreply.github.com>
2020-06-11 14:23:51 +0100

    Update ccloud to latest ksqlDB syntax

--- ccloud/statements.sql
+++ ccloud/statements.sql
@@ -1,5 +1,5 @@
 CREATE STREAM pageviews_original WITH (kafka_topic='pageviews', value_format='AVRO');
-CREATE TABLE users_original (userId STRING PRIMARY KEY) WITH (kafka_topic='users', value_format='AVRO');
+CREATE TABLE users_original (key_userId STRING PRIMARY KEY) WITH (kafka_topic='users', value_format='AVRO');
 CREATE STREAM pageviews_female AS SELECT users_original.userid AS userid, pageid, regionid, gender FROM pageviews_original LEFT JOIN users_original ON pageviews_original.userid = users_original.userid WHERE gender = 'FEMALE';
 CREATE STREAM pageviews_female_like_89 AS SELECT * FROM pageviews_female WHERE regionid LIKE '%_8' OR regionid LIKE '%_9';
 CREATE TABLE pageviews_regions AS SELECT gender, regionid , COUNT(*) AS numusers FROM pageviews_female WINDOW TUMBLING (size 30 second) GROUP BY gender, regionid HAVING COUNT(*) > 1;
