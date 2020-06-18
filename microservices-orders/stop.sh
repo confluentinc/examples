0897b9f352dd969d99a5b103e2e4cf76fde9fe86
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-06-18 17:07:19 -0500

    DEVX-1871 microsvcs: silence errors in stop routine

--- microservices-orders/stop.sh
+++ microservices-orders/stop.sh
@@ -5,9 +5,9 @@ source ../utils/helper.sh
 
 check_env || exit 1
 
-./scripts/kill-services.sh .microservices.pids
-rm .microservices.pids
+./scripts/kill-services.sh .microservices.pids 2> /dev/null
+rm .microservices.pids 2> /dev/null
 confluent local destroy 2> /dev/null
 
 # This is used in the services
-rm -fr /tmp/kafka-streams
+rm -fr /tmp/kafka-streams 2> /dev/null
