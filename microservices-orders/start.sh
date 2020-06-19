0c5e3290e9ccc57897b30a705b071e468255a524
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-06-19 10:57:56 -0500

    DEVX-1871: microservices: tidy and refactor to prepare for cloud (#681)

--- microservices-orders/start.sh
+++ microservices-orders/start.sh
@@ -10,6 +10,7 @@ check_netstat || exit 1
 check_running_elasticsearch 5.6.16 || exit 1
 check_running_kibana || exit 1
 check_running_cp ${CONFLUENT} || exit 1
+check_sqlite3 || exit 1
 
 ./stop.sh
 
@@ -17,70 +18,58 @@ get_and_compile_kafka_streams_examples || exit 1
 
 confluent-hub install --no-prompt confluentinc/kafka-connect-jdbc:$CONFLUENT
 confluent-hub install --no-prompt confluentinc/kafka-connect-elasticsearch:$CONFLUENT
-echo "auto.offset.reset=earliest" >> $CONFLUENT_HOME/etc/ksqldb/ksql-server.properties
+grep -qxF 'auto.offset.reset=earliest' $CONFLUENT_HOME/etc/ksqldb/ksql-server.properties || echo 'auto.offset.reset=earliest' >> $CONFLUENT_HOME/etc/ksqldb/ksql-server.properties 
 confluent local start
 sleep 5
 
-# Get random port number
-RESTPORT=$(jot -r 1  10000 65000)
-COUNT=0
-while [[ $(netstat -ant | grep $RESTPORT) != "" ]]; do
-  RESTPORT=$(jot -r 1  10000 65000)
-  COUNT=$((COUNT+1))
-  if [[ $COUNT > 5 ]]; then
-    echo "Could not allocate a free rest port. Please troubleshoot"
-    exit 1
-  fi
-done
-echo "Port tcp:$RESTPORT looks free"
+export BOOTSTRAP_SERVER=localhost:9092
+export SCHEMA_REGISTRY_URL=http://localhost:8081
+export SQLITE_DB_PATH=${PWD}/db/data/microservices.db
+export ELASTICSEARCH_URL=http://localhost:9200
 
-# Create topics
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic orders
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic order-validations
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic warehouse-inventory
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic customers
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic payments
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic platinum
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic gold
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic silver
-kafka-topics --create --zookeeper localhost:2181 --partitions 1 --replication-factor 1 --topic bronze
+echo "Creating demo topics"
+./scripts/create-topics.sh
 
-echo "Starting OrdersService"
-(cd kafka-streams-examples && mvn exec:java -f pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.OrdersService -Dexec.args="localhost:9092 http://localhost:8081 localhost $RESTPORT" > /dev/null 2>&1 &)
-sleep 10
-if [[ $(netstat -ant | grep $RESTPORT) == "" ]]; then
-  echo "OrdersService not running on port $RESTPORT.  Please troubleshoot"
-  exit 1
-fi
+echo "Setting up sqlite DB"
+(cd db; sqlite3 data/microservices.db < ./customers.sql)
 
-echo "Adding Inventory"
-COUNT_UNDERPANTS=25
-COUNT_JUMPERS=20
-(cd kafka-streams-examples && mvn exec:java -f pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.AddInventory -Dexec.args="$COUNT_UNDERPANTS $COUNT_JUMPERS" > /dev/null 2>&1 &)
-
-# Kafka Connect to source customers from sqlite3 database and produce to Kafka topic "customers"
-prep_sqltable_customers
-if check_cp; then confluent local config jdbc-customers -- -d ./connectors/connector_jdbc_customers.config; else confluent local config jdbc-customers -- -d ./connectors/connector_jdbc_customers_oss.config; fi
-
-# Sink Connector -> Elasticsearch -> Kibana
-if check_cp; then confluent local config elasticsearch -- -d ./connectors/connector_elasticsearch.config; else confluent local config elasticsearch -- -d ./connectors/connector_elasticsearch_oss.config; fi
+echo "Configuring Elasticsearch and Kibana"
 ./dashboard/set_elasticsearch_mapping.sh
 ./dashboard/configure_kibana_dashboard.sh
 
-# Start microservices
-for SERVICE in "InventoryService" "FraudService" "OrderDetailsService" "ValidationsAggregatorService" "EmailService"; do
-    echo "Starting $SERVICE"
-    (cd kafka-streams-examples && mvn exec:java -f pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.$SERVICE > /dev/null 2>&1 &)
+echo ""
+echo "Submitting connectors"
+
+# Kafka Connect to source customers from sqlite3 database and produce to Kafka topic "customers"
+INPUT_FILE=./connectors/connector_jdbc_customers_template.config 
+OUTPUT_FILE=./connectors/rendered-connectors/connector_jdbc_customers.config 
+source ./scripts/render-connector-config.sh
+confluent local config jdbc-customers -- -d $OUTPUT_FILE 2> /dev/null
+
+# Sink Connector -> Elasticsearch -> Kibana
+INPUT_FILE=./connectors/connector_elasticsearch_template.config
+OUTPUT_FILE=./connectors/rendered-connectors/connector_elasticsearch.config
+source ./scripts/render-connector-config.sh
+confluent local config elasticsearch -- -d $OUTPUT_FILE 2> /dev/null
+
+# Find an available local port to bind the REST service to
+FREE_PORT=$(jot -r 1  10000 65000)
+COUNT=0
+while [[ $(netstat -ant | grep "$FREE_PORT") != "" ]]; do
+  FREE_PORT=$(jot -r 1  10000 65000)
+  COUNT=$((COUNT+1))
+  if [[ $COUNT > 5 ]]; then
+    echo "Could not allocate a free network port. Please troubleshoot"
+    exit 1
+  fi
 done
+echo "Port $FREE_PORT looks free for the Orders Service"
+echo "Running Microservices, logging info in .microservices.log and running pids in .microservices.pids"
+( RESTPORT=$FREE_PORT JAR=$(pwd)"/kafka-streams-examples/target/kafka-streams-examples-$CONFLUENT-standalone.jar" scripts/run-services.sh > .microservices.log 2>&1 & )
 
-sleep 10
-
-echo -e "\nPosting Order Requests and Payments"
-(cd kafka-streams-examples && mvn exec:java -f pom.xml -Dexec.mainClass=io.confluent.examples.streams.microservices.PostOrdersAndPayments -Dexec.args="$RESTPORT" > /dev/null 2>&1 &)
-
-sleep 10
-
-# Create KSQL queries
+echo "Waiting for data population before starting ksqlDB applications"
+sleep 150
+# Create ksqlDB queries
 ksql http://localhost:8088 <<EOF
 run script 'statements.sql';
 exit ;
--- microservices-orders/stop.sh
+++ microservices-orders/stop.sh
@@ -5,8 +5,9 @@ source ../utils/helper.sh
 
 check_env || exit 1
 
-jps | grep Launcher | awk '{print $1;}' | xargs kill -9
-confluent local destroy
+./scripts/kill-services.sh .microservices.pids 2> /dev/null
+rm .microservices.pids 2> /dev/null
+confluent local destroy 2> /dev/null
 
 # This is used in the services
-rm -fr /tmp/kafka-streams
+rm -fr /tmp/kafka-streams 2> /dev/null
