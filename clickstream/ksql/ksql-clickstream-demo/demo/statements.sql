215900189562c3f4245683780ae75c77551a529b
Andy Coates <8012398+big-andy-coates@users.noreply.github.com>
2020-07-06 12:56:41 +0100

    Update the clickstream demo to work with ksqlDB 0.10.0, (which will be part of CP 6.0.0). (#670)
    Tasks done:
    * updated the SQL in `statements.sql` to be compliant with latest syntax
    * restructured `statements.sql` to make it easier to follow (hopefully)
    * Changed the `CLICK_USER_SESSIONS` table to track `WINDOWEND` as the timestamp that ES will use, as this means each new event has a new timestamp, allowing:
    * Changed the Grafana dashboard so that the User Sessions table actually shows the events-per-session over time. (See screen shots below).
    * Updated instructions slightly, including using ksqlDB `PRINT` to inspect topics, and adding an image of what it should look like.
    
    ### New dashboard shot included in the instructions (which show new session data too):
    ![New dashboard shot](clickstream/docs/images/grafana-success.png)
    
    ### Old 'Sessions' dashboard shot:
    ![Old sessions shot](https://github.com/confluentinc/examples/blob/5.5.0-post/clickstream/docs/images/grafana-success.png)
    
    ### New 'Sessions' dashboard shot:
    ![New sessions shot](clickstream/docs/images/grafana-sessions.png)
    
    Co-authored-by: Andy Coates <big-andy-coates@users.noreply.github.com>

--- clickstream/ksql/ksql-clickstream-demo/demo/statements.sql
+++ clickstream/ksql/ksql-clickstream-demo/demo/statements.sql
@@ -1,64 +1,162 @@
--- 1. SOURCE of ClickStream
-CREATE STREAM clickstream (_time bigint,time varchar, ip varchar, request varchar, status int, userid int, bytes bigint, agent varchar) with (kafka_topic = 'clickstream', value_format = 'json');
+---------------------------------------------------------------------------------------------------
+-- Create sources:
+---------------------------------------------------------------------------------------------------
+-- stream of user clicks:
+CREATE STREAM clickstream (
+        _time bigint,
+        time varchar,
+        ip varchar,
+        request varchar,
+        status int,
+        userid int,
+        bytes bigint,
+        agent varchar
+    ) with (
+        kafka_topic = 'clickstream',
+        value_format = 'json'
+    );
 
+-- error code lookup table:
+CREATE TABLE clickstream_codes (
+        code int primary key,
+        definition varchar
+    ) with (
+        kafka_topic = 'clickstream_codes',
+        value_format = 'json'
+    );
 
-----------------------------------------------------------------------------------------------------------------------------
--- A series of basic clickstream-analytics
---
--- Min, Max, UDFs etc
-----------------------------------------------------------------------------------------------------------------------------
+-- users lookup table:
+CREATE TABLE WEB_USERS (
+        user_id int primary key,
+        registered_At BIGINT,
+        username varchar,
+        first_name varchar,
+        last_name varchar,
+        city varchar,
+        level varchar
+    ) with (
+        kafka_topic = 'clickstream_users',
+        value_format = 'json'
+    );
 
- -- number of events per minute - think about key-for-distribution-purpose - shuffling etc - shouldnt use 'userid'
-CREATE table events_per_min AS SELECT userid, WINDOWSTART as EVENT_TS, count(*) AS events FROM clickstream window TUMBLING (size 60 second) GROUP BY userid;
+---------------------------------------------------------------------------------------------------
+-- Build materialized stream views:
+---------------------------------------------------------------------------------------------------
 
--- 3. BUILD STATUS_CODES
--- static table
-CREATE TABLE clickstream_codes (rowkey INTEGER KEY, code int, definition varchar) with (key='code', kafka_topic = 'clickstream_codes', value_format = 'json');
+-- enrich click-stream with more user information:
+CREATE STREAM USER_CLICKSTREAM AS
+    SELECT
+        userid,
+        u.username,
+        ip,
+        u.city,
+        request,
+        status,
+        bytes
+    FROM clickstream c
+        LEFT JOIN web_users u ON c.userid = u.user_id;
 
--- 4. BUILD PAGE_VIEWS
-CREATE TABLE pages_per_min AS SELECT userid, WINDOWSTART as EVENT_TS, count(*) AS pages FROM clickstream WINDOW HOPPING (size 60 second, advance by 5 second) WHERE request like '%html%' GROUP BY userid ;
+-- Enrich click-stream with more information about error codes:
+CREATE STREAM ENRICHED_ERROR_CODES AS
+    SELECT
+        code,
+        definition
+    FROM clickstream
+        LEFT JOIN clickstream_codes ON clickstream.status = clickstream_codes.code;
 
-----------------------------------------------------------------------------------------------------------------------------
--- URL STATUS CODES (Join AND Alert)
---
---
-----------------------------------------------------------------------------------------------------------------------------
+---------------------------------------------------------------------------------------------------
+-- Build materialized table views:
+---------------------------------------------------------------------------------------------------
 
--- Use 'HAVING' Filter to show ERROR codes > 400 where count > 5
-CREATE TABLE ERRORS_PER_MIN_ALERT AS SELECT status, WINDOWSTART as EVENT_TS, count(*) AS errors FROM clickstream window HOPPING ( size 30 second, advance by 20 second) WHERE status > 400 GROUP BY status HAVING count(*) > 5 AND count(*) is not NULL;
+-- Per-userId tables -----------------------------------------------------------------------------
 
-CREATE table ERRORS_PER_MIN AS SELECT status, WINDOWSTART as EVENT_TS, count(*) AS errors FROM clickstream window HOPPING ( size 60 second, advance by 5  second) WHERE status > 400 GROUP BY status;
+-- Table of events per minute for each user:
+CREATE table events_per_min AS
+    SELECT
+        userid as k1,
+        AS_VALUE(userid) as userid,
+        WINDOWSTART as EVENT_TS,
+        count(*) AS events
+    FROM clickstream window TUMBLING (size 60 second)
+    GROUP BY userid;
 
---Join using a STREAM
-CREATE STREAM ENRICHED_ERROR_CODES AS SELECT code, definition FROM clickstream LEFT JOIN clickstream_codes ON clickstream.status = clickstream_codes.code;
+-- Table of html pages per minute for each user:
+CREATE TABLE pages_per_min AS
+    SELECT
+        userid as k1,
+        AS_VALUE(userid) as userid,
+        WINDOWSTART as EVENT_TS,
+        count(*) AS pages
+    FROM clickstream WINDOW HOPPING (size 60 second, advance by 5 second)
+    WHERE request like '%html%'
+    GROUP BY userid;
+
+-- Per-username tables ----------------------------------------------------------------------------
+
+-- User sessions table - 30 seconds of inactivity expires the session
+-- Table counts number of events within the session
+CREATE TABLE CLICK_USER_SESSIONS AS
+    SELECT
+        username as K,
+        AS_VALUE(username) as username,
+        WINDOWEND as EVENT_TS,
+        count(*) AS events
+    FROM USER_CLICKSTREAM window SESSION (30 second)
+    GROUP BY username;
+
+-- Per-status tables ------------------------------------------------------------------------------
+
+-- number of errors per min, using 'HAVING' Filter to show ERROR codes > 400 where count > 5
+CREATE TABLE ERRORS_PER_MIN_ALERT AS
+    SELECT
+        status as k1,
+        AS_VALUE(status) as status,
+        WINDOWSTART as EVENT_TS,
+        count(*) AS errors
+    FROM clickstream window HOPPING (size 60 second, advance by 20 second)
+    WHERE status > 400
+    GROUP BY status
+    HAVING count(*) > 5 AND count(*) is not NULL;
+
+-- number of errors per min
+CREATE TABLE ERRORS_PER_MIN AS
+    SELECT
+        status as k1,
+        AS_VALUE(status) as status,
+        WINDOWSTART as EVENT_TS,
+        count(*) AS errors
+    FROM clickstream window HOPPING (size 60 second, advance by 5  second)
+    WHERE status > 400
+    GROUP BY status;
+
+-- Per-error code tables --------------------------------------------------------------------------
+
+-- Enriched error codes table:
 -- Aggregate (count&groupBy) using a TABLE-Window
-CREATE TABLE ENRICHED_ERROR_CODES_COUNT AS SELECT code, WINDOWSTART as EVENT_TS, definition, COUNT(*) AS count FROM ENRICHED_ERROR_CODES WINDOW TUMBLING (size 30 second) GROUP BY code, definition HAVING COUNT(*) > 1;
+CREATE TABLE ENRICHED_ERROR_CODES_COUNT AS
+    SELECT
+        code as k1,
+        definition as K2,
+        AS_VALUE(code) as code,
+        WINDOWSTART as EVENT_TS,
+        AS_VALUE(definition) as definition,
+        COUNT(*) AS count
+    FROM ENRICHED_ERROR_CODES WINDOW TUMBLING (size 30 second)
+    GROUP BY code, definition
+    HAVING COUNT(*) > 1;
 
-----------------------------------------------------------------------------------------------------------------------------
--- Clickstream users for enrichment and exception monitoring
---
---
-----------------------------------------------------------------------------------------------------------------------------
-
--- users lookup table
-CREATE TABLE WEB_USERS (rowkey INTEGER KEY, user_id int, registered_At BIGINT, username varchar, first_name varchar, last_name varchar, city varchar, level varchar) with (key='user_id', kafka_topic = 'clickstream_users', value_format = 'json');
-
-----------------------------------------------------------------------------------------------------------------------------
--- User experience monitoring
---
--- View IP, username and City Versus web-site-activity (hits)
-----------------------------------------------------------------------------------------------------------------------------
-CREATE STREAM USER_CLICKSTREAM AS SELECT userid, u.username, ip, u.city, request, status, bytes FROM clickstream c LEFT JOIN web_users u ON c.userid = u.user_id;
-
+-- Enriched user details table:
 -- Aggregate (count&groupBy) using a TABLE-Window
-CREATE TABLE USER_IP_ACTIVITY AS SELECT username, WINDOWSTART as EVENT_TS, ip, city, COUNT(*) AS count FROM USER_CLICKSTREAM WINDOW TUMBLING (size 60 second) GROUP BY username, ip, city HAVING COUNT(*) > 1;
-
-----------------------------------------------------------------------------------------------------------------------------
--- User session monitoring
---
--- Sessionisation using IP addresses - 300 seconds of inactivity expires the session
---
-----------------------------------------------------------------------------------------------------------------------------
-
-CREATE TABLE CLICK_USER_SESSIONS AS SELECT username, WINDOWSTART as EVENT_TS, count(*) AS events FROM USER_CLICKSTREAM window SESSION (30 second) GROUP BY username;
-
+CREATE TABLE USER_IP_ACTIVITY AS
+    SELECT
+        username as k1,
+        ip as k2,
+        city as k3,
+        AS_VALUE(username) as username,
+        WINDOWSTART as EVENT_TS,
+        AS_VALUE(ip) as ip,
+        AS_VALUE(city) as city,
+        COUNT(*) AS count
+    FROM USER_CLICKSTREAM WINDOW TUMBLING (size 60 second)
+    GROUP BY username, ip, city
+    HAVING COUNT(*) > 1;
