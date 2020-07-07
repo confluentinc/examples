684a8455fd62a53e9fab455ece86a45e4615e92c
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-07-07 13:03:27 -0500

    DEVX-1881: microservices-orders updated bootstrap-servers cli arg

--- microservices-orders/scripts/run-services.sh
+++ microservices-orders/scripts/run-services.sh
@@ -18,23 +18,23 @@ echo "Starting microservices from $JAR"
 echo "Connecting to cluster @ $BOOTSTRAP_SERVER and Schema Registry @ $SCHEMA_REGISTRY_URL"
 
 echo "Starting OrdersService"
-java -cp $JAR io.confluent.examples.streams.microservices.OrdersService --bootstrap-server $BOOTSTRAP_SERVER --schema-registry $SCHEMA_REGISTRY_URL --port $RESTPORT $CONFIG_FILE_ARG $ADDITIONAL_ARGS >$LOG_DIR/OrdersService.log 2>&1 &
+java -cp $JAR io.confluent.examples.streams.microservices.OrdersService --bootstrap-servers $BOOTSTRAP_SERVER --schema-registry $SCHEMA_REGISTRY_URL --port $RESTPORT $CONFIG_FILE_ARG $ADDITIONAL_ARGS >$LOG_DIR/OrdersService.log 2>&1 &
 PIDS+=($!)
 sleep 10
 
 echo "Adding Inventory"
-java -cp $JAR io.confluent.examples.streams.microservices.AddInventory --bootstrap-server $BOOTSTRAP_SERVER $CONFIG_FILE_ARG $ADDITIONAL_ARGS >$LOG_DIR/AddInventory.log 2>&1 &
+java -cp $JAR io.confluent.examples.streams.microservices.AddInventory --bootstrap-servers $BOOTSTRAP_SERVER $CONFIG_FILE_ARG $ADDITIONAL_ARGS >$LOG_DIR/AddInventory.log 2>&1 &
 sleep 5
 
 for SERVICE in "InventoryService" "FraudService" "OrderDetailsService" "ValidationsAggregatorService" "EmailService"; do
   echo "Starting $SERVICE"
-  java -cp $JAR io.confluent.examples.streams.microservices.$SERVICE --bootstrap-server $BOOTSTRAP_SERVER --schema-registry $SCHEMA_REGISTRY_URL $CONFIG_FILE_ARG $ADDITIONAL_ARGS >$LOG_DIR/$SERVICE.log 2>&1 &
+  java -cp $JAR io.confluent.examples.streams.microservices.$SERVICE --bootstrap-servers $BOOTSTRAP_SERVER --schema-registry $SCHEMA_REGISTRY_URL $CONFIG_FILE_ARG $ADDITIONAL_ARGS >$LOG_DIR/$SERVICE.log 2>&1 &
   PIDS+=($!)
 done
 sleep 10
 
 echo "Posting Orders and Payments"
-java -cp $JAR io.confluent.examples.streams.microservices.PostOrdersAndPayments --bootstrap-server $BOOTSTRAP_SERVER --schema-registry $SCHEMA_REGISTRY_URL --order-service-url "http://localhost:$RESTPORT" $CONFIG_FILE_ARG >$LOG_DIR/PostOrdersAndPayments.log 2>&1 &
+java -cp $JAR io.confluent.examples.streams.microservices.PostOrdersAndPayments --bootstrap-servers $BOOTSTRAP_SERVER --schema-registry $SCHEMA_REGISTRY_URL --order-service-url "http://localhost:$RESTPORT" $CONFIG_FILE_ARG >$LOG_DIR/PostOrdersAndPayments.log 2>&1 &
 
 PIDS+=($!)
 sleep 10
