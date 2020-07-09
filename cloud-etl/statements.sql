7926ac208ad7c52d326642e56624bdfecfb8397d
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-07-09 12:03:08 -0500

    DEVX-1912 cloud-etl Update ksqlDB create table statement
    to restore inclusion of eventSourceIP in the materialized value record & schema

--- cloud-etl/statements.sql
+++ cloud-etl/statements.sql
@@ -1,3 +1,3 @@
 CREATE STREAM eventlogs (eventSourceIP varchar, eventAction varchar, Result varchar, eventDuration bigint) WITH (kafka_topic='eventlogs', value_format='JSON');
 CREATE TABLE count_per_source WITH (KAFKA_TOPIC='COUNT_PER_SOURCE', PARTITIONS=6) AS SELECT eventSourceIP, count(*) as COUNT FROM eventlogs GROUP BY eventSourceIP EMIT CHANGES;
-CREATE TABLE sum_per_source WITH (KAFKA_TOPIC='SUM_PER_SOURCE', PARTITIONS=6, VALUE_FORMAT='AVRO') AS SELECT eventSourceIP, sum(eventDuration) as SUM FROM eventlogs WHERE Result='Pass' GROUP BY eventSourceIP EMIT CHANGES;
+CREATE TABLE sum_per_source WITH (KAFKA_TOPIC='SUM_PER_SOURCE', PARTITIONS=6, VALUE_FORMAT='AVRO') AS SELECT eventSourceIP ROWKEY, as_value(eventSourceIP) EVENTSOURCEIP, SUM(EVENTDURATION) SUM FROM eventlogs WHERE (RESULT = 'Pass') GROUP BY EVENTSOURCEIP EMIT CHANGES;
