f00205c3ec769951364ff038ef63e208aebb7c13
Yeva Byzek <yeva@confluent.io>
2020-09-18 16:14:58 -0400

    Consume works

--- clients/cloud/rest-proxy/consume.sh
+++ clients/cloud/rest-proxy/consume.sh
@@ -1,30 +1,34 @@
 #!/bin/bash
   
-CONFIG_FILE=$HOME/.confluent/java.config
-
-source ../../../utils/helper.sh
-
 # Set topic name
-topic_name=test3
+topic_name=${topic_name:-test3}
+
+cg_name=cg1
+ci=ci1
 
 # Create a consumer for JSON data, starting at the beginning of the topic's log
 docker-compose exec rest-proxy curl -X POST \
      -H "Content-Type: application/vnd.kafka.v2+json" \
-     --data '{"name": "my_consumer_instance", "format": "json", "auto.offset.reset": "earliest"}' \
-     http://localhost:8082/consumers/my_json_consumer
+     --data '{"name": "'"$ci"'", "format": "json", "auto.offset.reset": "earliest"}' \
+     http://localhost:8082/consumers/$cg_name
 
 # Subscribe to a topic
 docker-compose exec rest-proxy curl -X POST \
      -H "Content-Type: application/vnd.kafka.v2+json" \
-     --data '{"topics":["$topic_name"]}' \
-     http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance/subscription
+     --data '{"topics":["'"$topic_name"'"]}' \
+     http://localhost:8082/consumers/$cg_name/instances/$ci/subscription
 
 # Consume some data using the base URL in the first response.
 docker-compose exec rest-proxy curl -X GET \
      -H "Accept: application/vnd.kafka.json.v2+json" \
-     http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance/records
+     http://localhost:8082/consumers/$cg_name/instances/$ci/records
 
-# Close the consumer with a DELETE to make it leave the group and clean up its resources.
+# Consume some data using the base URL in the first response.
+docker-compose exec rest-proxy curl -X GET \
+     -H "Accept: application/vnd.kafka.json.v2+json" \
+     http://localhost:8082/consumers/$cg_name/instances/$ci/records
+
+# Close the consumer with a DELETE to make it leave the group and clean up its resources
 docker-compose exec rest-proxy curl -X DELETE \
      -H "Content-Type: application/vnd.kafka.v2+json" \
-     http://localhost:8082/consumers/my_json_consumer/instances/my_consumer_instance
+     http://localhost:8082/consumers/$cg_name/instances/$ci
