491a0ca02193086bf976efcb00791850c2ffd1ab
Rick Spurgeon <rspurgeon@users.noreply.github.com>
2020-09-25 11:57:35 -0500

    DEVX-2123: Delay reading ksqlDB topics during validation until available (#805)
    * DEVX-2123: Delay reading ksqlDB topics during validation until available
    
    * DEVX-2123: Delay reading ksqlDB topics during validation until available

--- microservices-orders/read-topics-docker.sh
+++ microservices-orders/read-topics-docker.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+source ../utils/helper.sh
+
 ################################
 
 echo -e "\n*** Sampling messages in Kafka topics and a KSQL stream ***\n"
@@ -32,6 +34,12 @@ docker-compose exec connect kafka-console-consumer --bootstrap-server broker:909
 # Topic platinum: dynamic routing
 echo -e "\n-----platinum-----"
 docker-compose exec connect kafka-avro-console-consumer --bootstrap-server broker:9092 --topic platinum --property print.key=true --property key.deserializer=org.apache.kafka.common.serialization.StringDeserializer --property schema.registry.url=http://schema-registry:8081 --from-beginning --max-messages 3 --timeout-ms 10000
+
+echo -e "\n-----waiting for ksqlDB topics-----"
+retry 60 check_topic_exists connect broker:9092 ORDERS_CUST1_JOINED || {
+	echo "ksqlDB topics were not created, debug and try again"
+	exit 1
+}
 
 # Read queries
 docker-compose exec ksqldb-cli bash -c "ksql http://ksqldb-server:8088 <<EOF
