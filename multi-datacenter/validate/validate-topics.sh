039893e48a9e75f02e6583f68b645b6a9a425700
Yeva Byzek <yeva@confluent.io>
2020-10-29 08:41:00 -0400

    Tweak

--- multi-datacenter/validate/validate-topics.sh
+++ multi-datacenter/validate/validate-topics.sh
@@ -2,6 +2,6 @@
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
 
-echo -e "multi-datacenter example: validate that the script at ${DIR}/../read-topics.sh completes"
+echo -e "Validate that the script at ${DIR}/../read-topics.sh completes"
 
 ${DIR}/../read-topics.sh
