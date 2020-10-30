932e96f325b8b000970689f1b81ec020c2893983
Yeva Byzek <yeva@confluent.io>
2020-10-30 13:55:40 -0400

    Extract retired Pumba Docker commands into pumba.txt

--- multiregion/scripts/latency_docker.sh
+++ multiregion/scripts/latency_docker.sh
@@ -4,7 +4,7 @@
 # This code injects latency between the regions and packet loss to simulate the WAN link
 # by configuring 'tc' commands directly on ZooKeeper and broker containers
 #
-# Running Pumba is an alternate method but is commented out because:
+# Running Pumba (see pumba.txt) is an alternate method but:
 # - Pumba can't run 'tc' on the Confluent containers because Docker containers run as 'appuser', not 'root'
 # - Pumba could run with '--tc-image gaiadocker/iproute2' flag (does not require 'tc' on Docker containers)
 #   but that works only on the first run and fails on subsequent runs, requiring a Docker restart in between runs
@@ -21,15 +21,6 @@ export KAFKA_EAST_4_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{
 export SUBNET=$(docker inspect multiregion_n1 -f '{{(index .IPAM.Config 0).Subnet}}')
 
 echo -e "\n==> Configuring central as a medium latency link (50ms)"
-#docker run --rm -d \
-#        --network=${DOCKER_NETWORK} \
-#        --name pumba-medium-latency-central \
-#        -v /var/run/docker.sock:/var/run/docker.sock \
-#        -e SUBNET=${SUBNET} \
-#        gaiaadm/pumba:0.7.6 -l debug netem --duration 24000h \
-#                --tc-image gaiadocker/iproute2 \
-#                --target $SUBNET \
-#                delay --time 50 zookeeper-central --jitter 10 &
 docker-compose exec -u0 zookeeper-central tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 zookeeper-central tc qdisc add dev eth0 parent 1:1 handle 10: sfq > /dev/null
 docker-compose exec -u0 zookeeper-central tc qdisc add dev eth0 parent 1:2 handle 20: sfq > /dev/null
@@ -37,19 +28,6 @@ docker-compose exec -u0 zookeeper-central tc qdisc add dev eth0 parent 1:3 handl
 docker-compose exec -u0 zookeeper-central tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst $SUBNET flowid 1:3 > /dev/null
 
 echo -e "\n==> Configuring west-east as a high latency link (100ms)"
-#docker run --rm -d \
-#        --network=${DOCKER_NETWORK} \
-#        --name pumba-high-latency-west-east \
-#        -v /var/run/docker.sock:/var/run/docker.sock \
-#        -e ZOOKEEPER_EAST_IP=${ZOOKEEPER_EAST_IP} \
-#        -e KAFKA_EAST_3_IP=${KAFKA_EAST_3_IP} \
-#        -e KAFKA_EAST_4_IP=${KAFKA_EAST_4_IP} \
-#        gaiaadm/pumba:0.7.6 -l debug netem --duration 24000h \
-#                --tc-image gaiadocker/iproute2 \
-#                --target ${ZOOKEEPER_EAST_IP} \
-#                --target ${KAFKA_EAST_3_IP} \
-#                --target ${KAFKA_EAST_4_IP} \
-#                delay --time 100 zookeeper-west broker-west-1 broker-west-2 --jitter 20 &
 docker-compose exec -u0 zookeeper-west tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 broker-west-2 tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 broker-west-1 tc qdisc add dev eth0 root handle 1: prio > /dev/null
@@ -73,19 +51,6 @@ docker-compose exec -u0 broker-west-1 tc filter add dev eth0 protocol ip parent 
 docker-compose exec -u0 zookeeper-west tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst $KAFKA_EAST_4_IP flowid 1:3 > /dev/null
 
 echo -e "\n==> Configuring west-east with 1% packet loss"
-#docker run --rm -d \
-#        --network=${DOCKER_NETWORK} \
-#        --name pumba-loss-west-east \
-#        -v /var/run/docker.sock:/var/run/docker.sock \
-#        -e ZOOKEEPER_EAST_IP=${ZOOKEEPER_EAST_IP} \
-#        -e KAFKA_EAST_3_IP=${KAFKA_EAST_3_IP} \
-#        -e KAFKA_EAST_4_IP=${KAFKA_EAST_4_IP} \
-#        gaiaadm/pumba:0.7.6 -l debug netem --duration 24000h \
-#                --tc-image gaiadocker/iproute2 \
-#                --target ${ZOOKEEPER_EAST_IP} \
-#                --target ${KAFKA_EAST_3_IP} \
-#                --target ${KAFKA_EAST_4_IP} \
-#                loss --percent 1 zookeeper-west broker-west-1 broker-west-2 &
 docker-compose exec -u0 broker-west-2 tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 broker-west-1 tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 zookeeper-west tc qdisc add dev eth0 root handle 1: prio > /dev/null
@@ -109,19 +74,6 @@ docker-compose exec -u0 broker-west-1 tc filter add dev eth0 protocol ip parent 
 docker-compose exec -u0 broker-west-2 tc filter add dev eth0 protocol ip parent 1:0 prio 1 u32 match ip dst $KAFKA_EAST_4_IP flowid 1:3 > /dev/null
 
 echo -e "\n==> Configuring east-west with 1% packet loss"
-#docker run --rm -d \
-#        --network=${DOCKER_NETWORK} \
-#        --name pumba-loss-east-west \
-#        -v /var/run/docker.sock:/var/run/docker.sock \
-#        -e ZOOKEEPER_WEST_IP=${ZOOKEEPER_WEST_IP} \
-#        -e KAFKA_WEST_1_IP=${KAFKA_WEST_1_IP} \
-#        -e KAFKA_WEST_2_IP=${KAFKA_WEST_2_IP} \
-#        gaiaadm/pumba:0.7.6 -l debug netem --duration 24000h \
-#                --tc-image gaiadocker/iproute2 \
-#                --target ${ZOOKEEPER_WEST_IP} \
-#                --target ${KAFKA_WEST_1_IP} \
-#                --target ${KAFKA_WEST_2_IP} \
-#                loss --percent 1 zookeeper-east broker-east-3 broker-east-4 &
 docker-compose exec -u0 zookeeper-east tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 broker-east-4 tc qdisc add dev eth0 root handle 1: prio > /dev/null
 docker-compose exec -u0 broker-east-3 tc qdisc add dev eth0 root handle 1: prio > /dev/null
