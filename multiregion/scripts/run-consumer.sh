b8c1f5bd7751cf8137b9c5b6271b8b8ba44c48f4
Yeva Byzek <yeva@confluent.io>
2020-11-24 13:29:05 -0500

    Remove threads from kafka-consumer-perf-test due to KAFKA-10126

--- multiregion/scripts/run-consumer.sh
+++ multiregion/scripts/run-consumer.sh
@@ -4,7 +4,6 @@ echo -e "\n\n==> Consume from east: Multi-region Async Replication reading from 
 
 docker-compose exec broker-east-3 kafka-consumer-perf-test --topic multi-region-async \
     --messages 5000 \
-    --threads 1 \
     --broker-list broker-west-1:19091,broker-east-3:19093 \
     --timeout 20000
 
@@ -12,28 +11,24 @@ echo -e "\n\n==> Consume from east: Multi-region Async Replication reading from 
 
 docker-compose exec broker-east-3 kafka-consumer-perf-test --topic multi-region-async \
     --messages 5000 \
-    --threads 1 \
     --broker-list broker-west-1:19091,broker-east-3:19093 \
     --timeout 20000 \
     --consumer.config /etc/kafka/demo/consumer-east.config
 
 docker-compose exec broker-east-3 kafka-consumer-perf-test --topic multi-region-async-op-under-min-isr \
     --messages 5000 \
-    --threads 1 \
     --broker-list broker-west-1:19091,broker-east-3:19093 \
     --timeout 20000 \
     --consumer.config /etc/kafka/demo/consumer-east.config
 
 docker-compose exec broker-east-3 kafka-consumer-perf-test --topic multi-region-async-op-under-replicated \
     --messages 5000 \
-    --threads 1 \
     --broker-list broker-west-1:19091,broker-east-3:19093 \
     --timeout 20000 \
     --consumer.config /etc/kafka/demo/consumer-east.config
 
 docker-compose exec broker-east-3 kafka-consumer-perf-test --topic multi-region-async-op-leader-is-observer \
     --messages 5000 \
-    --threads 1 \
     --broker-list broker-west-1:19091,broker-east-3:19093 \
     --timeout 20000 \
     --consumer.config /etc/kafka/demo/consumer-east.config
