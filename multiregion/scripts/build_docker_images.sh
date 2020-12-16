aeef944bbedc50695e952df2eea727966774f029
Yeva Byzek <yeva@confluent.io>
2020-12-16 00:35:23 -0500

    DEVX-2341: build_docker_images.sh does not run from scripts directory

--- multiregion/scripts/build_docker_images.sh
+++ multiregion/scripts/build_docker_images.sh
@@ -7,5 +7,5 @@ source ${DIR}/../.env
 echo
 echo "Build custom cp-zookeeper and cp-server images with 'tc' installed"
 for image in cp-zookeeper cp-server; do
-  docker build --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=${REPOSITORY} --build-arg IMAGE=$image -t localbuild/${image}-tc:${CONFLUENT_DOCKER_TAG} -f Dockerfile .
+  docker build --build-arg CP_VERSION=${CONFLUENT_DOCKER_TAG} --build-arg REPOSITORY=${REPOSITORY} --build-arg IMAGE=$image -t localbuild/${image}-tc:${CONFLUENT_DOCKER_TAG} -f ${DIR}/../Dockerfile ${DIR}/../.
 done
