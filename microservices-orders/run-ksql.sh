902922cb3abd79e9a935f71ec036ec58820fefcc
awalther28 <12937975+awalther28@users.noreply.github.com>
2021-01-20 15:22:17 -0700

    DEVX-2434: move to functions

--- microservices-orders/run-ksql.sh
+++ microservices-orders/run-ksql.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
+source /tmp/helper.sh
 
-echo -e "\nWaiting for ksqlDB and topics to be available prior to running ksqlDB statements\n"
-while [[ $(curl -s http://ksqldb-server:8088/info) != *RUNNING* \
-             ||  $(curl -s http://schema-registry:8081/subjects) != *customers* \
-             ||  $(curl -s http://schema-registry:8081/subjects) != *orders* ]]
-do 
-    sleep 5
-    echo "Sleeping for 5 seconds, waiting for ksqlDB and topics to be available"
-done
+echo -e "\nWaiting for ksqlDB to be RUNNING prior to executing ksqlDB statements\n"
+retry 300 check_ksqlDB_host_running || exit 1
 
+echo -e "\nksqlDB host is running, now check that necessary topics have been created"
+array=("customers" "orders")
+retry 200 check_schema_registry_topics_exists "${array[@]}" || exit 1
 
 # Create KSQL queries
+echo -e "\nNecessary topics have been created, now run ksqlDB statements"
 ksql http://ksqldb-server:8088 <<EOF
 run script '/tmp/statements.sql';
 exit ;
