1208b4e002bb87a369302028ced87b9145aeb714
awalther28 <12937975+awalther28@users.noreply.github.com>
2021-01-20 09:03:35 -0700

    DEVX-2434: update to check for topics actually needed

--- microservices-orders/run-ksql.sh
+++ microservices-orders/run-ksql.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 echo -e "\nWaiting for ksqlDB and topics to be available prior to running ksqlDB statements\n"
-while [[ $(curl -s http://ksqldb-server:8088/info) != *RUNNING* ||  $(curl -s http://schema-registry:8081/subjects) != *platinum* ]]
+while [[ $(curl -s http://ksqldb-server:8088/info) != *RUNNING* \
+             ||  $(curl -s http://schema-registry:8081/subjects) != *customers* \
+             ||  $(curl -s http://schema-registry:8081/subjects) != *orders* ]]
 do 
     sleep 5
     echo "Sleeping for 5 seconds, waiting for ksqlDB and topics to be available"
