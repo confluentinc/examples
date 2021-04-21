78fc5c395467024bc0b903e58dcf0d32402975c4
awalther28 <12937975+awalther28@users.noreply.github.com>
2021-04-21 10:37:48 -0600

    DEVX-2235: pin ES

--- clickstream/start.sh
+++ clickstream/start.sh
@@ -7,7 +7,7 @@ source ../utils/helper.sh
 
 # Get jars for source and sink connectors
 docker run -v $PWD/confluent-hub-components:/share/confluent-hub-components confluentinc/ksqldb-server:0.8.0 confluent-hub install --no-prompt confluentinc/kafka-connect-datagen:$KAFKA_CONNECT_DATAGEN_VERSION
-docker run -v $PWD/confluent-hub-components:/share/confluent-hub-components confluentinc/ksqldb-server:0.8.0 confluent-hub install --no-prompt confluentinc/kafka-connect-elasticsearch:10.0.2
+docker run -v $PWD/confluent-hub-components:/share/confluent-hub-components confluentinc/ksqldb-server:0.8.0 confluent-hub install --no-prompt confluentinc/kafka-connect-elasticsearch:$KAFKA_CONNECT_ES_VERSION
 
 docker-compose up -d
 
