3873ab9b1c41a790a21dbdc156d373ac73fc5b42
Jan Sulmont <modality@gmail.com>
2021-09-08 17:13:17 +0200

    producer NewTopic ctor call + cleanup

--- clients/cloud/clojure/src/io/confluent/examples/clients/clj/consumer.clj
+++ clients/cloud/clojure/src/io/confluent/examples/clients/clj/consumer.clj
@@ -11,28 +11,28 @@
 (defn- build-properties [config-fname]
   (with-open [config (jio/reader config-fname)]
     (doto (Properties.)
-      (.putAll {ConsumerConfig/GROUP_ID_CONFIG, "clojure_example_group"
-                ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"
+      (.putAll {ConsumerConfig/GROUP_ID_CONFIG,                "clojure_example_group"
+                ConsumerConfig/KEY_DESERIALIZER_CLASS_CONFIG   "org.apache.kafka.common.serialization.StringDeserializer"
                 ConsumerConfig/VALUE_DESERIALIZER_CLASS_CONFIG "org.apache.kafka.common.serialization.StringDeserializer"})
       (.load config))))
 
 (defn consumer! [config-fname topic]
   (with-open [consumer (KafkaConsumer. (build-properties config-fname))]
     (.subscribe consumer [topic])
-    (loop [tc 0
+    (loop [tc      0
            records []]
       (let [new-tc (reduce
-                    (fn [tc record]
-                      (let [value (.value record)
-                            cnt (get (json/read-str value) "count")
-                            new-tc (+ tc cnt)]
-                        (printf "Consumed record with key %s and value %s, and updated total count to %d\n"
-                                (.key record)
-                                value
-                                new-tc)
-                        new-tc))
-                        tc
-                        records)]
+                     (fn [tc record]
+                       (let [value  (.value record)
+                             cnt    (get (json/read-str value) "count")
+                             new-tc (+ tc cnt)]
+                         (printf "Consumed record with key %s and value %s, and updated total count to %d\n"
+                                 (.key record)
+                                 value
+                                 new-tc)
+                         new-tc))
+                     tc
+                     records)]
         (println "Waiting for message in KafkaConsumer.poll")
         (recur new-tc
                (seq (.poll consumer (Duration/ofSeconds 1))))))))
