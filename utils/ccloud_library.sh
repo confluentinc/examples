0f41ce13f95435cdef862aa590521f839e3fe5c9
Yeva Byzek <yeva@confluent.io>
2021-09-22 08:13:45 -0400

    Allow ENVIRONMENT reuse

--- utils/ccloud_library.sh
+++ utils/ccloud_library.sh
@@ -1035,7 +1035,8 @@ function ccloud::destroy_ccloud_stack() {
   fi
 
   SERVICE_ACCOUNT_ID=$1
-  ENVIRONMENT=$(ccloud::get_environment_id_from_service_id $SERVICE_ACCOUNT_ID)
+  ENVIRONMENT=${ENVIRONMENT:-$(ccloud::get_environment_id_from_service_id $SERVICE_ACCOUNT_ID)}
+
   ccloud environment use $ENVIRONMENT || exit 1
 
   PRESERVE_ENVIRONMENT="${PRESERVE_ENVIRONMENT:-false}"
