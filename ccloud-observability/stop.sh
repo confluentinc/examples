345a02545b73d9f135f85755ad1ef66542b9b474
Yeva Byzek <ybyzek@users.noreply.github.com>
2022-01-13 09:21:57 -0500

    DEVX-2704: CCloud observability demo should handle error on API key c… (#1062)
    * DEVX-2704: CCloud observability demo should handle error on API key creation
    
    * Delete METRICS_API_KEY in stop.sh for better cleanup

--- ccloud-observability/stop.sh
+++ ccloud-observability/stop.sh
@@ -1,9 +1,15 @@
 #!/bin/bash
 
+if [ -z "$METRICS_API_KEY" ]; then
+  echo "ERROR: Must export parameter METRICS_API_KEY before running this script to destroy the API key created for metrics."
+  exit 1
+fi
+
 if [ -z "$1" ]; then
   echo "ERROR: Must supply argument that is the client configuration file created from './start.sh'. (Is it in stack-configs/ folder?) "
   exit 1
 else
+  confluent api-key delete $METRICS_API_KEY
   docker-compose down -v
   DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
   CONFIG_FILE=${DIR}/$1
