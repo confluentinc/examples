7b83a13a505db4e87b7aac5b110b32b5a8c5f099
Yeva Byzek <yeva@confluent.io>
2022-02-23 20:33:53 -0500

    Update max version to 2.6.1

--- utils/helper.sh
+++ utils/helper.sh
@@ -48,7 +48,7 @@ function validate_version_confluent_cli_for_cp() {
   validate_version_confluent_cli_v2 || exit 1
 
   VER_MIN="2.3.1"
-  VER_MAX="2.3.1"
+  VER_MAX="2.6.1"
   CLI_VERSION=$(get_version_confluent_cli)
 
   if version_gt $VER_MIN $CLI_VERSION || version_gt $CLI_VERSION $VER_MAX ; then
